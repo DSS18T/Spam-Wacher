9c9e34c39fc4157605838fcd733781b79f837cde
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-14 00:03:17 +0530

    Create __init__.py

---
+++ Nandha/__init__.py
@@ -0,0 +1,17 @@
+from pyrogram import Client
+
+
+APP_ID = 583311
+APP_HASH = "c67090d2c0a37d207385d04d9640045c"
+TOKEN = "5720769813:AAEMCkgcEB-t0J7MVFr75QG4lshVwMHQ7O8"
+
+Nandha = Client(
+"Nandha", 
+api_id=int(APP_ID), 
+api_hash=str(APP_HAS), 
+bot_token=str(TOKEN),
+plugins=dict(root="Nandha"))
+
+
+
+     
