3cc6633b835e241056789af0cfb9368b5c69c008
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-14 20:52:17 +0530

    Update __main__.py

--- Nandha/__main__.py
+++ Nandha/__main__.py
@@ -3,6 +3,11 @@ import config
 from Nandha import Nandha
 
 
+
+@Nandha.on_message(filters.command("start",config.CMDS))
+async def start(_, message):
+     await message.reply("`Group Protection Alive!`")
+
 if __name__ == "__main__":
     Nandha.run()
     with Nandha:
