0a994155323c61c24bb6c688b41976644e9b101d
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-14 19:36:20 +0530

    Update bans.py

--- Nandha/plugins/bans.py
+++ Nandha/plugins/bans.py
@@ -9,7 +9,7 @@ async def bans(_, message):
       user_id = int(message.from_user.id)
       chat_id = int(message.chat.id)
       reply = message.reply_to_message
-      if (await can_ban_members(chat_id, user_id)) == True or user_id in config.OWNER_ID and reply:    
+      if (await can_ban_members(chat_id, user_id)) == True or user_id == config.OWNER_ID and reply:    
            if reply and len(message.command) <2:
                 user_id = reply.from_user.id
            elif reply and len(message.command) >2:
