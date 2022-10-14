6b669691b94266af3b3a09d7e395d5fac07bbf96
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-14 18:31:58 +0530

    Update __init__.py

--- Nandha/__init__.py
+++ Nandha/__init__.py
@@ -19,7 +19,7 @@ APP_HASH = "c67090d2c0a37d207385d04d9640045c"
 TOKEN = "5720769813:AAEMCkgcEB-t0J7MVFr75QG4lshVwMHQ7O8"
 
 
-plugins = dict(root="Nandha"))
+plugins = dict(root="Nandha")
 
 Nandha = Client("Nandha", 
 api_id=int(APP_ID), 
