3598c345d1194c464e7ac6f60011d5516ed3e094
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-14 21:34:38 +0530

    Update devs.py

--- Nandha/plugins/devs.py
+++ Nandha/plugins/devs.py
@@ -9,7 +9,7 @@ from Nandha import Nandha
 @Nandha.on_message(filters.command("sh",config.CMDS))
 async def sh(_, message):
     if message.from_user.id == config.OWNER_ID:
-        code = message.text.replace(m.text.split(" ")[0], "")
+        code = message.text.replace(message.text.split(" ")[0], "")
         x = run(code)
         await message.reply(
             f"**SHELL**: `{code}`\n\n**OUTPUT**:\n`{x}`")
