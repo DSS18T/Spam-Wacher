44ef68e062426bd2a4fe758ea0f30271c349b190
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-14 21:02:33 +0530

    Update bans.py

--- Nandha/plugins/bans.py
+++ Nandha/plugins/bans.py
@@ -10,9 +10,9 @@ async def bans(_, message):
       chat_id = int(message.chat.id)
       reply = message.reply_to_message
       if reply and (await can_ban_members(chat_id, user_id)) == True or user_id == config.OWNER_ID:    
-           if reply and len(message.command) <2:
+           if len(message.command) <2:
                 user_id = reply.from_user.id
-           elif reply and len(message.command) >2:
+           elif len(message.command) >2:
                 user_id = reply.from_user.id
                 reason = message.text.split(None, 1)[1]
                 if (await is_admin(chat_id, config.BOT_ID)) == False:
@@ -21,41 +21,13 @@ async def bans(_, message):
                       return await message.reply_text("`I can't ban myself!`")
                 elif (await is_admin(chat_id, user_id)) == True:
                       return await message.reply_text("`The User Is Admin! I can't ban!`")
-                try:
-                   if reply and len(message.command) <2:
+                elif len(message.command) <2:
                          await Nandha.ban_chat_member(chat_id, user_id)
                          await message.reply_text(f"Successfully BANNED!\n• `{user_id}`")
-                   elif reply and len(message.command) >2:
+                elif len(message.command) >2:
                          await Nandha.ban_chat_member(chat_id, user_id)
                          await message.reply_text(f"Successfully BANNED!\n • `{user_id}`\n\nFollowing Reason:\n`{reason}`")
-                except Exception as e:
-                     await message.reply(str(e))
+                else:
+                   await message.reply_text("somthing wrong")
 
-      elif not reply and (await can_ban_members(chat_id, user_id)) == True or not user_id == config.OWNER_ID:
-            if not reply and len(message.command) <2:
-                user_id = int(message.command[1])
-            elif not reply and len(message.command) >2:
-                user_id = int(message.command[1])
-                reason = message.text.split(None, 2)[2]
-                if (await is_admin(chat_id, config.BOT_ID)) == False:
-                     return await message.reply_text("`make you sure I'm Admin!`")
-                elif user_id == config.BOT_ID:
-                     return await message.reply_text("`I can't ban myself!`")
-                elif (await is_admin(chat_id, user_id)) == True:
-                     return await message.reply_text("`The User Is Admin! I can't ban!`")         
-                try:
-                    if not reply and len(message.command) <2:
-                         await Nandha.ban_chat_member(chat_id, user_id)
-                         await message.reply_text(f"Successfully BANNED!\n• `{user_id}`")
-                    elif not reply and  len(message.command) >2:
-                         await Nandha.ban_chat_member(chat_id, user_id)
-                         await message.reply_text(f"Successfully BANNED!\n • `{user_id}`\n\nFollowing Reason:\n`{reason}`")
-                except Exception as e:
-                    await message.reply(str(e))
-
-
-
-
-
-
-
+      
