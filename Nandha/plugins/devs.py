6c9767b211b8774055b08e41fb6e8df40cd48e6d
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-14 21:28:45 +0530

    Create devs.py

---
+++ Nandha/plugins/devs.py
@@ -0,0 +1,17 @@
+import config
+
+from subprocess import getoutput as run
+from pyrogram import filters
+from Nandha import Nandha
+
+
+
+@Nandha.on_message(filters.command("sh",config.CMDS))
+async def sh(_, message):
+    if message.from_user.id in config.OWNER_ID:
+        code = message.text.replace(m.text.split(" ")[0], "")
+        x = run(code)
+        await message.reply(
+            f"**SHELL**: `{code}`\n\n**OUTPUT**:\n`{x}`")
+    else:
+        await message.reply("`Only Devs can Access this command!`")
