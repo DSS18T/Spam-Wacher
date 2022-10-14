efa83eccc1be949e1eb64dc65643aeda2ebeb98d
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-14 17:32:10 +0530

    Update __main__.py

--- Nandha/__main__.py
+++ Nandha/__main__.py
@@ -4,4 +4,4 @@ from Nandha import Nandha
 if __name__ == "__main__":
     Nandha.run()
     with Nandha:
-        Nandha.send_message(config.GROUP_ID, "`hello every your group protection system alive!`")
+        Nandha.send_message(config.GROUP_ID, "`hello everyone!\nyour group protection system awakened!`")
