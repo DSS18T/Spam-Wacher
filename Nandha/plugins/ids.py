390575693f423ec8d060f8f9d769124ec675687e
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-15 14:25:10 +0530

    Update ids.py

--- Nandha/plugins/ids.py
+++ Nandha/plugins/ids.py
@@ -10,18 +10,18 @@ async def ids(_, message):
       if reply:
          id = "`here the file ids`:\n\n"
          id += f"**Chat ID**: `{message.chat.id}`\n"
-         id += f"**Replied ID**: `{reply.from_user.id}`"
+         id += f"**Replied ID**: `{reply.from_user.id}`\n"
          id += f"**Your ID**: `{message.from_user.id}`\n"
          if reply.forward_from:
              id += f"**Forward From ID**:\n`{message.forward_from.id}`\n"
          elif reply.photo:
-             id += f"**Photo ID**:\n\n{reply.photo.file_id}"
+             id += f"**Photo ID**:\n`{reply.photo.file_id}`"
          elif reply.animation:
-             id += f"**Animation ID**:\n\n{reply.animation.file_id}"
+             id += f"**Animation ID**:\n`{reply.animation.file_id}`"
          elif reply.audio:
-             id += f"**Audio ID**:\n\n{reply.audio.file_id}"
+             id += f"**Audio ID**:\n`{reply.audio.file_id}`"
          elif reply.sticker:
-             id += f"**Sticker ID**:\n\n{reply.sticker.file_id}"
+             id += f"**Sticker ID**:\n`{reply.sticker.file_id}`"
          await message.reply(text=(id))
 
          
