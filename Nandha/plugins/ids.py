5ea7e25d81579be6e8530854b15d90b0aa68d219
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-15 14:33:32 +0530

    Update ids.py

--- Nandha/plugins/ids.py
+++ Nandha/plugins/ids.py
@@ -14,6 +14,14 @@ async def ids(_, message):
          id += f"**Your ID**: `{message.from_user.id}`\n"
          if reply.forward_from:
              id += f"**Forward From ID**:\n`{reply.forward_from.id}`\n"
+         elif reply.forward_from.photo:
+             id += f"**Photo ID**:\n`{reply.forward_from.photo.file_id}`"
+         elif reply.forward_from.animation:
+             id += f"**Animation ID**:\n`{reply.forward_from.animation.file_id}`"
+         elif reply.forward_from.audio:
+             id += f"**Audio ID**:\n`{reply.forward_from.audio.file_id}`"
+         elif reply.forward_from.sticker:
+             id += f"**Sticker ID**:\n`{reply.forward_from.sticker.file_id}`"
          elif reply.photo:
              id += f"**Photo ID**:\n`{reply.photo.file_id}`"
          elif reply.animation:
