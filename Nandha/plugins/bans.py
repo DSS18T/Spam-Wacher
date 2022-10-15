95f96f5f9f5706faf9b87d9c122b6c500560e17b
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-15 11:36:40 +0530

    Update bans.py

--- Nandha/plugins/bans.py
+++ Nandha/plugins/bans.py
@@ -11,10 +11,18 @@ async def bans(_, message):
       reply = message.reply_to_message
       if (await can_ban_members(chat_id,user_id)) == True: 
           await message.reply_text("yeh banning....")  
-          if len(message.command) >1:
+          if not reply and len(message.command) >2:
               ban_id = int(message.text.split(" ")[1])
-          else:
-              ban_id = int(reply.from_user.id)
+              reason = message.text.split(None, 2)[2]
+          elif not reply and len(message.command) == 2:
+               ban_id = int(message.text.split(" ")[1])
+               reason = None
+          elif reply and len(message.command) >1:
+              ban_id = reply.from_user.id
+              reason = message.text.split(None, 1)[1]
+          elif reply and len(message.command) <2:
+               ban_id = reply_from_user.id
+               reason = None
           if (await is_admin(chat_id, config.BOT_ID)) == False:
                    return await message.reply_text("`Make you sure I'm Admin!`")
           elif ban_id == config.BOT_ID:
@@ -22,10 +30,6 @@ async def bans(_, message):
           elif (await is_admin(chat_id, ban_id)) == True:
                    return await message.reply_text("`The User Is Admin! I can't ban!`")
           else:
-              if len(message.text.split()) >1:
-                   reason = message.text.split(None, 1)[1]
-              else:
-                   reason = None
               await Nandha.ban_chat_member(chat_id, ban_id)
               await message.reply_text(f"Successfully BANNED!\n • `{ban_id}`\n\nFollowing Reason:\n`{reason}`")
                      
