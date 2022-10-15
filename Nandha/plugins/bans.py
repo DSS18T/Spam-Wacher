f550f5fb647ae758e676d216200f05b7911acb56
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-15 17:06:30 +0530

    Update bans.py

--- Nandha/plugins/bans.py
+++ Nandha/plugins/bans.py
@@ -31,14 +31,11 @@ async def bans(_, message):
           elif (await is_admin(chat_id, ban_id)) == True:
                    return await message.reply_text("`The User Is Admin! I can't ban!`")
           else:
-             try:
                await message.reply_sticker(random.choice(config.FUNNY_STICKER))
                await Nandha.ban_chat_member(chat_id, ban_id)
                await message.reply_text(f"The Bitch As Dust!\n • `{ban_id}`\n\nFollowing Reason:\n`{reason}`",
                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Unban", callback_data=f"unban_btn:{ban_id}")]]))
-             except Exception as e: 
-                await message.reply_text(str(e))  
-
+             
 
 @Nandha.on_callback_query(filters.regex("unban_btn"))
 async def unban_btn(_, query):
