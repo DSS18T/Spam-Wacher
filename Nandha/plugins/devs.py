7615deecdfbb74beb843281e70f58977758b1251
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-15 10:36:23 +0530

    Update devs.py

--- Nandha/plugins/devs.py
+++ Nandha/plugins/devs.py
@@ -8,10 +8,18 @@ from Nandha import Nandha
 
 @Nandha.on_message(filters.command("sh",config.CMDS))
 async def sh(_, message):
-    if message.from_user.id == config.OWNER_ID:
-        code = message.text.replace(message.text.split(" ")[0], "")
-        x = run(code)
-        await message.reply(
-            f"**SHELL**: `{code}`\n\n**OUTPUT**:\n`{x}`")
+    if not message.from_user.id == config.OWNER_ID:
+          return await message.reply_text("`You Don't Have Rights To Run This!`")
+    elif len(message.command) <2:
+         await message.reply_text("`No Input Found!`")
     else:
-        await message.reply("`Only Devs can Access this command!`")
+          code = message.text.replace(message.text.split(" ")[0], "")
+          x = run(code)
+          string = f"**📎 Input**: `{code}`\n\n**📒 Output **:\n`{x}`")
+          if len(code) >4000:
+              with io.BytesIO(str.encode(string)) as out_file:
+                 out_file.name = "shell.text"
+                 await message.reply_document(document=out_file, caption="`LONG TEXT MESSAGE CANNOT SHOW SO WE SEND FILE TYPE.`")
+          else:
+             await message.reply_text(string) 
+               
