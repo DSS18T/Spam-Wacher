8f081dd67227e96000e99e7c9c95444ae96b8ab1
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-15 20:20:25 +0530

    Create lock.py

---
+++ Nandha/plugins/lock.py
@@ -0,0 +1,20 @@
+import config
+
+from pyrogram import filters
+from pyrogram.types import *
+from Nandha import Nandha
+from Nandha.help.admin import *
+
+@Nandha.on_message(filters.command("lock",config.CMDS))
+async def lock(_, message):
+      chat_id = message.chat.id
+      user_id = message.from_user.id
+      try:
+           if (await is_admin(chat_id,user_id)) == True:
+                if len(message.text.split()) <2:
+                     return await message.reply("`Please input the locktypes.`")
+                locktypes = message.text.split()[1]
+                if locktypes in "all":
+                    await Nandha.set_chat_permissions(chat_id, ChatPermissions())
+                    await message.reply("`locked all!`")
+                       
