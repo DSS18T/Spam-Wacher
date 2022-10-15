46810a24e3c512821edf853dcf5d744ffa30eb10
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-15 23:34:11 +0530

    Update lock.py

--- Nandha/plugins/lock.py
+++ Nandha/plugins/lock.py
@@ -17,16 +17,52 @@ async def lock(_, message):
                 if locktypes in "all":
                     await Nandha.set_chat_permissions(chat_id, ChatPermissions())
                     await message.reply("`locked all!`")
-                elif locktypes in "msg":
-                    await Nandha.set_chat_permissions(chat_id,ChatPermissions(can_send_messages=False))
-                    await message.reply("`locked messages!`")
+                elif locktypes in "invite":
+                     await Nandha.set_chat_permissions(chat_id,ChatPermissions(
+                      can_send_messages=True,
+                     can_send_media_messages=True,
+                     can_send_other_messages=True,
+                     can_add_web_page_previews=True,
+                     can_invite_users=False,
+                     can_send_polls=True ))
+                    await message.reply("`locked invite!`")
                 elif locktypes in "media":
                      await Nandha.set_chat_permissions(chat_id,ChatPermissions(
                      can_send_messages=True,
                      can_send_media_messages=False,
                      can_send_other_messages=False,
                      can_add_web_page_previews=True,
-                     can_invite_users=True))
+                     can_invite_users=True,
+                     can_send_polls=True))
                      await message.reply("`locked medias!`")
+                elif locktypes in "poll":
+                     await Nandha.set_chat_permissions(chat_id,ChatPermissions(
+                     can_send_messages=True,
+                     can_send_media_messages=True,
+                     can_send_other_messages=True,
+                     can_add_web_page_previews=True,
+                     can_invite_users=True,
+                     can_send_polls=False))
+                     await message.reply("`locked polls!`")         
       except Exception as e:         
           await message.reply(e)
+
+
+
+LOCKTYPES = """
+`you can lock this list of chat permissions!`
+
+• `all`: for lock (chat all permissions)
+• `media`: for lock chat (medias and other kid of msgs)
+• `poll`: for lock chat (sending polls)
+• `invite`: for lock (inviting users)
+"""
+
+@Nandha.on_message(filters.command("locktypes"))
+async def locktypes(_, message):
+     if (await is_admin(message.chat.id, message.from_user.id)) == True:
+          await message.reply_text(LOCKTYPES)
+     else:
+        await message.reply_text("`Admins Only!`")
+
+
