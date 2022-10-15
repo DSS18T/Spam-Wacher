57df595a87cb479187927a6f90356fc549b467f9
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-15 09:27:36 +0530

    Update bans.py

--- Nandha/plugins/bans.py
+++ Nandha/plugins/bans.py
@@ -22,11 +22,11 @@ async def bans(_, message):
                    return await message.reply_text("`I can't ban myself!`")
               elif (await is_admin(chat_id, ban_id)) == True:
                    return await message.reply_text("`The User Is Admin! I can't ban!`")
-              else:
-                  if len(message.command) <2:
-                          await Nandha.ban_chat_member(chat_id, ban_id)
-                          await message.reply_text(f"Successfully BANNED!\n• `{ban_id}`")
-                  elif len(message.command) >1:
-                          await Nandha.ban_chat_member(chat_id, ban_id)
-                          await message.reply_text(f"Successfully BANNED!\n • `{ban_id}`\n\nFollowing Reason:\n`{reason}`")
               
+              else:
+                     if len(message.command) >1:
+                         await Nandha.ban_chat_member(chat_id, ban_id)
+                         await message.reply_text(f"Successfully BANNED!\n • `{ban_id}`\n\nFollowing Reason:\n`{reason}`")
+                     elif len(message.command) <2:
+                         await Nandha.ban_chat_member(chat_id, ban_id)
+                         await message.reply_text(f"Successfully BANNED!\n • `{ban_id}`")
