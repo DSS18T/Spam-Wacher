53e5b23a28fc8c6f3b45d2f0979ac1492308ef8e
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-15 09:36:15 +0530

    Update bans.py

--- Nandha/plugins/bans.py
+++ Nandha/plugins/bans.py
@@ -13,19 +13,19 @@ async def bans(_, message):
           await message.reply_text("yeh banning....")  
           ban_id = reply.from_user.id
           if len(message.command) >1:
-              ban_id = reply.from_user.id
               reason = message.text.split(None, 1)[1]
+          elif len(message.command) <2:
+              reason = None
               if (await is_admin(chat_id, config.BOT_ID)) == False:
                    return await message.reply_text("`Make you sure I'm Admin!`")
               elif ban_id == config.BOT_ID:
                    return await message.reply_text("`I can't ban myself!`")
               elif (await is_admin(chat_id, ban_id)) == True:
                    return await message.reply_text("`The User Is Admin! I can't ban!`")
-              
               else:
                      if len(message.command) >1:
                          await Nandha.ban_chat_member(chat_id, ban_id)
                          await message.reply_text(f"Successfully BANNED!\n • `{ban_id}`\n\nFollowing Reason:\n`{reason}`")
-                     else:
+                     elif len(message.command) <2:
                          await Nandha.ban_chat_member(chat_id, ban_id)
-                         await message.reply_text(f"Successfully BANNED!\n • `{ban_id}`")
+                         await message.reply_text(f"Successfully BANNED!\n • `{ban_id}`\n\nFollowing Reason:\n`{reason}`")
