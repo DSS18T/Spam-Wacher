faa808b0c62c8c567ae775de469943a83255d895
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-15 09:03:58 +0530

    Ok

--- Nandha/plugins/bans.py
+++ Nandha/plugins/bans.py
@@ -16,19 +16,17 @@ async def bans(_, message):
           elif len(message.command) >2:
               ban_id = reply.from_user.id
               reason = message.text.split(None, 1)[1]
-              try:
-                  if (await is_admin(chat_id, config.BOT_ID)) == False:
-                       return await message.reply_text("`Make you sure I'm Admin!`")
-                  elif ban_id == config.BOT_ID:
-                        return await message.reply_text("`I can't ban myself!`")
-                  elif (await is_admin(chat_id, ban_id)) == True:
-                      return await message.reply_text("`The User Is Admin! I can't ban!`")
-                  elif len(message.command) <2:
-                        await Nandha.ban_chat_member(chat_id, ban_id)
-                        await message.reply_text(f"Successfully BANNED!\n• `{ban_id}`")
+              if (await is_admin(chat_id, config.BOT_ID)) == False:
+                   return await message.reply_text("`Make you sure I'm Admin!`")
+              elif ban_id == config.BOT_ID:
+                   return await message.reply_text("`I can't ban myself!`")
+              elif (await is_admin(chat_id, ban_id)) == True:
+                   return await message.reply_text("`The User Is Admin! I can't ban!`")
+              else:
+                  if len(message.command) <2:
+                          await Nandha.ban_chat_member(chat_id, ban_id)
+                          await message.reply_text(f"Successfully BANNED!\n• `{ban_id}`")
                   elif len(message.command) >2:
-                        await Nandha.ban_chat_member(chat_id, ban_id)
-                        await message.reply_text(f"Successfully BANNED!\n • `{ban_id}`\n\nFollowing Reason:\n`{reason}`")
-              except Exception as e:
-                 await message.reply(str(e))
-      
+                          await Nandha.ban_chat_member(chat_id, ban_id)
+                          await message.reply_text(f"Successfully BANNED!\n • `{ban_id}`\n\nFollowing Reason:\n`{reason}`")
+              
