8509d577dd35ee0dd2414226af646c278d889312
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-16 09:16:08 +0530

    Create send_media_group.py

---
+++ Nandha/plugins/send_media_group.py
@@ -0,0 +1,22 @@
+from pyrogram.types import InputMediaPhoto, InputMediaVideo
+from Nandha import Nandha
+from pyrogram import filters
+
+
+
+URL = ["https://telegra.ph/file/24ed9f2ebe33bb03a8a5b.mp4",
+"https://telegra.ph/file/1b9ec1c2e0ee9b5e1d8a9.mp4",
+"https://telegra.ph/file/4e7422366b53f659d8c0d.mp4",
+"https://telegra.ph/file/a46ae89eda709764f1530.jpg"]
+
+@Nandha.on_message(filters.command("test"))
+async def tests(_, m):
+   for link in URL:
+      await Nandha.send_media_group(message.chat.id,
+    [
+        InputMediaPhoto(link),
+        InputMediaPhoto(link, caption="test 1"),
+        InputMediaVideo(link, caption="test 2"),
+        InputMediaVideo(link, caption="test 3")
+    ]
+)
