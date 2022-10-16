488196ff60ee1816b58d174ed63416b65f25d198
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-16 07:59:07 +0530

    Update lock.py

--- Nandha/plugins/lock.py
+++ Nandha/plugins/lock.py
@@ -10,58 +10,108 @@ async def lock(_, message):
       chat_id = message.chat.id
       user_id = message.from_user.id
       try:
-           if (await is_admin(chat_id,user_id)) == True:
+          if (await is_admin(chat_id,user_id)) == True:
+                 
                 if len(message.text.split()) <2:
-                     return await message.reply("`Please input the locktypes.`")
-                locktypes = message.text.split()[1]
-                if locktypes in "all":
-                    await Nandha.set_chat_permissions(chat_id, ChatPermissions())
-                    await message.reply("`locked all!`")
-                elif locktypes in "invite":
-                     await Nandha.set_chat_permissions(chat_id,ChatPermissions(
-                     can_send_messages=True,
-                     can_send_media_messages=True,
-                     can_send_other_messages=True,
-                     can_add_web_page_previews=True,
-                     can_invite_users=False,
-                     can_send_polls=True ))
-                     await message.reply("`locked invite!`")
-                elif locktypes in "media":
-                     await Nandha.set_chat_permissions(chat_id,ChatPermissions(
-                     can_send_messages=True,
-                     can_send_media_messages=False,
-                     can_send_other_messages=False,
-                     can_add_web_page_previews=True,
-                     can_invite_users=True,
-                     can_send_polls=True))
-                     await message.reply("`locked medias!`")
-                elif locktypes in "poll":
-                     await Nandha.set_chat_permissions(chat_id,ChatPermissions(
-                     can_send_messages=True,
-                     can_send_media_messages=True,
-                     can_send_other_messages=True,
-                     can_add_web_page_previews=True,
-                     can_invite_users=True,
-                     can_send_polls=False))
-                     await message.reply("`locked polls!`")         
+                    return await message.reply("`No input found!`")
+
+                lock_type = message.text.split()[1]
+
+                get_perm = message.chat.permissions
+
+                msg = get_perm.can_send_messages
+                media = get_perm.can_send_media_messages
+                webprev = get_perm.can_add_web_page_previews
+                polls = get_perm.can_send_polls
+                info = get_perm.can_change_info
+                invite = get_perm.can_invite_users
+                pin = get_perm.can_pin_messages
+                stickers = animations = games = inlinebots = None
+                if lock_types in "all":
+                        await Nandha.set_chat_permissions(chat_id, ChatPermissions())
+                        await message.reply("`locked all`")
+                if lock_type in "msg":
+                    msg = False
+                    perm = "messages"
+
+                elif lock_type == "media":
+                     media = False
+                     perm = "audios, documents, photos, videos, video notes, voice notes"
+
+                elif lock_type == "stickers":
+                    stickers = False
+                    perm = "stickers"
+
+                elif lock_type == "animations":
+                     animations = False
+                     perm = "animations"
+
+                elif lock_type == "games":
+                     games = False
+                     perm = "games"
+
+                elif lock_type in ("inlinebots", "inline"):
+                     inlinebots = False
+                     perm = "inline bots"
+
+                elif lock_type == "webprev":
+                     webprev = False
+                     perm = "web page previews"
+
+                elif lock_type == "polls":
+                      polls = False
+                      perm = "polls"
+
+                elif lock_type == "info":
+                      info = False
+                      perm = "info"
+
+                elif lock_type == "invite":
+                      invite = False
+                      perm = "invite"
+
+                elif lock_type == "pin":
+                     pin = False
+                     perm = "pin"
+                else:
+                     return await message.reply("`Invalid locktype!`")
+                await Nandha.set_chat_permissions(
+                chat_id,
+                ChatPermissions(
+                can_send_messages=msg,
+                can_send_media_messages=media,
+                can_send_other_messages=any([stickers, animations, games, inlinebots]),
+                can_add_web_page_previews=webprev,
+                can_send_polls=polls,
+                can_change_info=info,
+                can_invite_users=invite,
+                can_pin_messages=pin,),)
+                await message.reply_text("`locked! {lock_type}`")
+                              
       except Exception as e:         
           await message.reply(e)
 
 
 
-LOCKTYPES = """
-`you can lock this list of chat permissions!`
-
-• `all`: for lock (chat all permissions)
-• `media`: for lock chat (medias and other kid of msgs)
-• `poll`: for lock chat (sending polls)
-• `invite`: for lock (inviting users)
-"""
 
 @Nandha.on_message(filters.command("locktypes"))
 async def locktypes(_, message):
      if (await is_admin(message.chat.id, message.from_user.id)) == True:
-          await message.reply_text(LOCKTYPES)
+          await message.reply_text(
+        (
+            "**Lock Types:**\n"
+            " - `all` = Everything\n"
+            " - `msg` = Messages\n"
+            " - `media` = Media, such as Photo and Video.\n"
+            " - `polls` = Polls\n"
+            " - `invite` = Add users to Group\n"
+            " - `pin` = Pin Messages\n"
+            " - `info` = Change Group Info\n"
+            " - `webprev` = Web Page Previews\n"
+            " - `inlinebots`, `inline` = Inline bots\n"
+            " - `animations` = Animations\n"
+            " - `games` = Game Bots\n"
+            " - `stickers` = Stickers"),)
      else:
         await message.reply_text("`Admins Only!`")
 
