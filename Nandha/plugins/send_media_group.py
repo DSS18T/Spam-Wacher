4674fa24d8ddcfd565fea8e4bc4b3352a4aa68dd
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-16 16:47:34 +0530

    Update send_media_group.py

--- Nandha/plugins/send_media_group.py
+++ Nandha/plugins/send_media_group.py
@@ -8,7 +8,6 @@ URL = (
 "https://telegra.ph/file/24ed9f2ebe33bb03a8a5b.mp4",
 "https://telegra.ph/file/1b9ec1c2e0ee9b5e1d8a9.mp4",
 "https://telegra.ph/file/4e7422366b53f659d8c0d.mp4",
-"https://telegra.ph/file/a46ae89eda709764f1530.jpg",
 )
 
 @Nandha.on_message(filters.command("test"))
