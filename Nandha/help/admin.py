1feee5088c5d11ff563d73ab87e96007674088dc
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-16 23:42:30 +0530

    Update admin.py

--- Nandha/help/admin.py
+++ Nandha/help/admin.py
@@ -12,3 +12,24 @@ async def can_ban_members(chat_id: int, user_id: int):
      if admin.privileges.can_restrict_members:
          return True
      return False
+
+async def can_promote_members(chat_id: int, user_id: int):
+     admin = await Nandha.get_chat_member(chat_id, user_id)
+     if admin.privileges.can_promote_members:
+         return True
+     return False
+
+async def can_promote_members(chat_id: int, user_id: int):
+     admin = await Nandha.get_chat_member(chat_id, user_id)
+     if admin.privileges.can_promote_members:
+         return True
+     return False
+
+async def can_change_info(chat_id: int, user_id: int):
+     admin = await Nandha.get_chat_member(chat_id, user_id)
+     if admin.privileges.can_change_info:
+         return True
+     return False
+
+
+
