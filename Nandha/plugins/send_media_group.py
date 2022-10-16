622e2f83bf41d195257f6b13ec1f545d392446b8
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-16 09:24:34 +0530

    Update send_media_group.py

--- Nandha/plugins/send_media_group.py
+++ Nandha/plugins/send_media_group.py
@@ -4,10 +4,12 @@ from pyrogram import filters
 
 
 
-URL = ["https://telegra.ph/file/24ed9f2ebe33bb03a8a5b.mp4",
+URL = (
+"https://telegra.ph/file/24ed9f2ebe33bb03a8a5b.mp4",
 "https://telegra.ph/file/1b9ec1c2e0ee9b5e1d8a9.mp4",
 "https://telegra.ph/file/4e7422366b53f659d8c0d.mp4",
-"https://telegra.ph/file/a46ae89eda709764f1530.jpg"]
+"https://telegra.ph/file/a46ae89eda709764f1530.jpg",
+)
 
 @Nandha.on_message(filters.command("test"))
 async def tests(_, m):
@@ -16,7 +18,5 @@ async def tests(_, m):
     [
         InputMediaPhoto(link),
         InputMediaPhoto(link, caption="test 1"),
-        InputMediaVideo(link, caption="test 2"),
-        InputMediaVideo(link, caption="test 3")
     ]
 )
