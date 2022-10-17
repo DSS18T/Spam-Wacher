339d109f716c7ffbae16138b9000a0d433be7c48
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-17 08:20:26 +0530

    Update admins.py

--- Nandha/plugins/admins.py
+++ Nandha/plugins/admins.py
@@ -10,7 +10,9 @@ async def promoting(_, message):
        reply = message.reply_to_message
        chat_id = message.chat.id
        user_id = message.from_user.id
-       if (await can_promote_members(chat_id,user_id)) == True:
+       if (await can_promote_members(chat_id,user_id)) == False:
+            return await message.reply("`Admins Only!`")
+       else:
                 bot = await Nandha.get_chat_member(chat_id,config.BOT_ID)
                 if reply and len(message.text.split()) >1:
                      user_id = reply.from_user.id
@@ -23,8 +25,7 @@ async def promoting(_, message):
                       admin_title = message.text.split(None,2)[2]
                 elif not reply and len(message.text.split()) <3:
                      user_id = message.text.split()[1]
-                     admin_title = "Admin"
-                
+                     admin_title = "Admin"                
                 if (await is_admin(chat_id,config.BOT_ID)) == False:
                       await message.reply("`Make you sure I'm Admin!`")
                 elif (await can_promote_members(chat_id,config.BOT_ID)) == False:
@@ -35,5 +36,4 @@ async def promoting(_, message):
                      await message.chat.promote_member(user_id=user_id,privileges=bot.privileges)
                      await Nandha.set_administrator_title(chat_id, user_id, title=admin_title)
                      await message.reply(f"**Successfully Promoted**!\n**Following Admin Tile**:\n`{admin_title}`") 
-       else:
-         return await message.reply("`You Don't Have Enough Rights!`")
+       
