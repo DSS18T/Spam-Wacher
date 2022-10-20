5b4675fa45a5c6fd62c5278a5aee2dbafb05b69c
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-20 21:47:24 +0530

    Update encodes.py

--- Nandha/plugins/encodes.py
+++ Nandha/plugins/encodes.py
@@ -8,13 +8,15 @@ import base64
 async def encodes(_, message):
       if not message.reply_to_message or not message.reply_to_message.text:
          return await message.reply("`reply to a message text to encode/decode!`")
-      string = message.reply_to_message.text
+      date = message.reply_to_message.text
       try:
-         if startswith("en"):
-            encodedBytes = base64.b64encode(data.encode("utf-8"))
-            encodedStr = str(encodedBytes, "utf-8")
-            await message.reply(encodedStr)
-         elif ends with("de"):
-             decodedBytes = base64.b64decode(str(data))
-             decodedStr = str(decodedBytes, "utf-8")
-             await message.reply(decodedStr)
+          if startswith("en"):
+              encodedBytes = base64.b64encode(data.encode("utf-8"))
+              encodedStr = str(encodedBytes, "utf-8")
+              await message.reply(encodedStr)
+          elif ends with("de"):
+              decodedBytes = base64.b64decode(str(data))
+              decodedStr = str(decodedBytes, "utf-8")
+              await message.reply(decodedStr)
+      except Exception as e:
+         await message.reply(e)
