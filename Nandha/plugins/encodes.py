f3057bdc956852596cd077fef1e2d9e3e8d5b19e
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-20 22:11:57 +0530

    J

--- Nandha/plugins/encodes.py
+++ Nandha/plugins/encodes.py
@@ -7,7 +7,7 @@ import base64
 @Nandha.on_message(filters.command(["encode","decode"],config.CMDS))
 async def encodes(_, message):
       if not message.reply_to_message or not message.reply_to_message.text:
-         return await message.reply("`reply to a message text to encode/decode!`")   
+         return await message.reply("`reply to a message text to encode!`")   
       else:
           data = message.reply_to_message.text
           encodedBytes = base64.b64encode(data.encode("utf-8"))
