7a6bbabf10c680002f450c7ec1440f167b41a035
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-20 20:22:26 +0530

    Update admins.py

--- Nandha/plugins/admins.py
+++ Nandha/plugins/admins.py
@@ -16,8 +16,8 @@ async def purge(_, message):
     reply = message.reply_to_message
     if message.chat.type == enums.ChatType.PRIVATE:
            for ids in range(reply.id, message.id +0):
-             await Nandha.delete_messages(chat_id, ids)
-    return await message.reply(f"**Success Purged {len({ids})} messages!**")
+              await Nandha.delete_messages(chat_id, ids)
+           return await message.reply(f"**Success Purged!**")
     if (await is_admin(chat_id,user_id)) == False:
             return await message.reply("`Admins Only!`")
     elif (await can_delete_messages(chat_id,user_id)) == False:
