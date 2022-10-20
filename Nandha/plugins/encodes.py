5d81870314305dda2597df22a3630b2d386a8cc4
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-20 21:52:15 +0530

    Update encodes.py

--- Nandha/plugins/encodes.py
+++ Nandha/plugins/encodes.py
@@ -6,17 +6,17 @@ import base64
 
 @Nandha.on_message(filters.command(["encode,decode],config.CMDS))
 async def encodes(_, message):
-      if not message.reply_to_message or not message.reply_to_message.text:
-         return await message.reply("`reply to a message text to encode/decode!`")
-      date = message.reply_to_message.text
+      if not message.reply_to_message or is not message.reply_to_message.text:
+         return await message.reply("`reply to a message text to encode/decode!`")   
       try:
+      date = message.reply_to_message.text
           if startswith("en"):
-              encodedBytes = base64.b64encode(data.encode("utf-8"))
-              encodedStr = str(encodedBytes, "utf-8")
-              await message.reply(encodedStr)
+               encodedBytes = base64.b64encode(data.encode("utf-8"))
+               encodedStr = str(encodedBytes, "utf-8")
+               await message.reply(encodedStr)
           elif endswith("de"):
-              decodedBytes = base64.b64decode(str(data))
-              decodedStr = str(decodedBytes, "utf-8")
-              await message.reply(decodedStr)
+               decodedBytes = base64.b64decode(str(data))
+               decodedStr = str(decodedBytes, "utf-8")
+               await message.reply(decodedStr)
       except Exception as e:
          await message.reply(e)
