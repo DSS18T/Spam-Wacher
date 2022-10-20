d878c2c69a420bba26f2e8833a9685fbcae8b526
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-20 21:38:55 +0530

    Create encodes.py

---
+++ Nandha/plugins/encodes.py
@@ -0,0 +1,19 @@
+from Nandha import Nandha
+from pyrogram import filters
+
+import base64
+
+@Nandha.on_message(filters.commamd(["encode,decode],config.CMDS))
+async def encodes(_, message):
+      if not message.reply_to_message or not message.reply_to_message.text:
+         return await message.reply("`reply to a message text to encode/decode!`")
+      string = message.reply_to_message.text
+      try:
+         if startswith("en"):
+            encodedBytes = base64.b64encode(data.encode("utf-8"))
+            encodedStr = str(encodedBytes, "utf-8")
+            await message.reply(encodedStr)
+         elif ends with("de"):
+             decodedBytes = base64.b64decode(str(data))
+             decodedStr = str(decodedBytes, "utf-8")
+             await message.reply(decodedStr)
