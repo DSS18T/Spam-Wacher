73fd46aae63e88e7ac75852a473ceeef91a9cf43
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-20 22:16:48 +0530

    Update encodes.py

--- Nandha/plugins/encodes.py
+++ Nandha/plugins/encodes.py
@@ -4,10 +4,10 @@ from pyrogram import filters
 
 import base64
 
-@Nandha.on_message(filters.command(["encode","decode"],config.CMDS))
+@Nandha.on_message(filters.command("encode",config.CMDS))
 async def encodes(_, message):
       if not message.reply_to_message or not message.reply_to_message.text:
-         return await message.reply("`reply to a message text to encode!`")   
+          return await message.reply("`reply to a message text to encode!`")   
       else:
           data = message.reply_to_message.text
           encodedBytes = base64.b64encode(data.encode("utf-8"))
