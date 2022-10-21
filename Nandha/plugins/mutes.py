da20cadd38d5174867f566992fdc8067819aba76
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-21 12:47:42 +0530

    Update mutes.py

--- Nandha/plugins/mutes.py
+++ Nandha/plugins/mutes.py
@@ -38,7 +38,7 @@ async def muted(_, message):
                 else:
                      await message.reply_sticker(random.choice(config.FUNNY_STICKER))
                      await Nandha.restrict_chat_member(chat_id, mute_id, ChatPermissions(can_send_messages=False))
-                     await message.reply_animation(url,caption=f"The Bitch Muted!\n • `{ban_id}`\n\nFollowing Reason:\n`{reason}`",
+                     await message.reply_animation(url,caption=f"The Bitch Muted!\n • `{mute_id}`\n\nFollowing Reason:\n`{reason}`",
                      reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Unmute", callback_data=f"unmute_btn:{mute_id}")]]))
       except Exception as e:
          await message.reply_text(e)
