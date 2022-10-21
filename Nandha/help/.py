ebd1aee49ab7ee3b166a05e1f623866ef1b6d1d0
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-21 21:36:40 +0530

    Create .py

---
+++ Nandha/help/.py
@@ -0,0 +1,8 @@
+from Nandha.help.aiohttp import post
+
+
+
+async def paste(text: str):
+     resp = await post(f"https://batbin.me/api/v2/paste", data=file_text)
+     code = resp["message"]
+     return f"https://batbin.me/{code}"
