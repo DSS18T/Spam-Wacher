e2424d582cd23f32bd6f50f208f918f3ddc02e8c
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-21 11:50:44 +0530

    Update encodes.py

--- Nandha/plugins/encodes.py
+++ Nandha/plugins/encodes.py
@@ -18,10 +18,10 @@ async def encodes(_, message):
              return await message.reply("`reply to text or give me some text to (en/de) code!`")
       elif not reply and len(message.text.split()) >1:
             data = message.text.split(None,1)[1]
-      if message.text.split()[0].startswith("en"):
+      if message.text.split()[0].startswith("/en"):
            encode = str(base64.b64encode(bytes(data, "utf-8")))
            await Nandha.send_message(chat_id, text=encode, reply_to_message_id=message.id)
-      elif message.text.split()[0].startswith("de"):
+      elif message.text.split()[0].startswith("/de"):
           decode = base64.b64decode(data).decode("utf-8", "ignore")
           await Nandha.send_message(chat_id, text=decode, reply_to_message_id=message.id)
 
