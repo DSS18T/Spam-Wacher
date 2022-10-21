f003d97b09f995bb313ab0941d76cdc43165bb4b
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-21 22:47:15 +0530

    Update misc.py

--- Nandha/plugins/misc.py
+++ Nandha/plugins/misc.py
@@ -19,17 +19,19 @@ async def ping(_, message):
 async def paste(_, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
-    if reply and reply.text or reply.caption:
-         text = reply.text or reply.caption
+    if reply and reply.text
+         text = reply.text 
+    elif reply and reply.caption:
+          text = reply.caption
     elif reply and reply.document:
           doc = await reply.download()
           async with aiofiles.open(doc, mode="r") as f:
                text = await f.read()
           os.remove(doc)
     elif not reply and len(message.text.split()) <2:
-         return await message.reply("`please reply to (text/file) or give text to paste!`")
+           return await message.reply("`please reply to (text/file) or give text to paste!`")
     elif not reply and len(message.text.split()) >1:
-         text = message.text.split(None, 1)[1]
+           text = message.text.split(None, 1)[1]
     link = await spacebin(text)
     await Nandha.send_message(chat_id,f"here paste:\n`{link}`",reply_to_message_id=message.id)
     
