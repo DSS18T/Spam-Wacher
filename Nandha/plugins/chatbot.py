41c086e91715c4473709ba91a4d3bdf396b83adf
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-21 23:52:49 +0530

    Update chatbot.py

--- Nandha/plugins/chatbot.py
+++ Nandha/plugins/chatbot.py
@@ -9,15 +9,22 @@ ACTIVE_CHAT = [
 123456789,
 987654321,]
 
-@Nandha.on_message(filters.command("chatbot",config.CMDS))
-async def chatbot(_, message):
+
+
+@Nandha.on_message(filters.command("addchatbot",config.CMDS))
+async def addchatbot(_, message):
     reply = message.reply_to_message
     if not message.chat.id in ACTIVE_CHAT:
             ACTIVE_CHAT.append(message.chat.id)
-            await message.reply("ChatBot Successfully Active!")
+            await message.reply("Successfully ChatBot Active!")
             return
-    if message.chat.id in ACTIVE_CHAT:
-         if message.reply_to_message.from_user.id == 5720769813:
+    else:
+        await message.reply("This Chat Already Enabled ChatBot!")
+    
+@Nandha.on_message(filters.text & group=200)
+async def chatbot(_, message):
+     if message.chat.id in ACTIVE_CHAT:
+          if message.reply_to_message.from_user.id == config.BOT_ID:
                Message = message.text
                API = requests.get("https://merissachatbot.tk/api/apikey=1491497760-MERISSAri2hds2WK4/groupprotectionbot/nandhaxd/message="+Message).json()
                await message.reply(API["reply"])
