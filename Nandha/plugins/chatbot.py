b6efbac44b54d6c548453c3d658a20bd35e760d3
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-21 23:31:37 +0530

    Create chatbot.py

---
+++ Nandha/plugins/chatbot.py
@@ -0,0 +1,21 @@
+import config
+from Nandha import Nandha
+from pyrogram import filters
+
+
+ACTIVE_CHAT = [
+123456789,
+987654321,]
+
+@Nandha.on_message(filters.command("chatbot",config.CMDS))
+async def chatbot(_, message):
+    reply = message.reply_to_message
+    if not message.chat.id in ACTIVE_CHAT:
+            ACTIVE_CHAT.append(message.chat.id)
+            await message.reply("ChatBot Successfully Active!")
+            return
+    if message.chat.id in ACTIVE_CHAT:
+         if reply.from_user.id == config.BOT_ID:
+                Message = message.text
+                API = requests.get(f"https://merissachatbot.tk/api/apikey=1491497760-MERISSAri2hds2WK4/groupprotectionbot/nandhaxd/message="+Message).json()
+                await message.reply(API["reply"])
