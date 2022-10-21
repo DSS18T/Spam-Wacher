9fb2721e29396df86acc4365f15463dcb7335f74
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-21 21:42:44 +0530

    Update paste.py

--- Nandha/help/paste.py
+++ Nandha/help/paste.py
@@ -3,6 +3,6 @@ from Nandha.help.aiohttp import post
 
 
 async def paste(text: str):
-     resp = await post(f"https://batbin.me/api/v2/paste", data=file_text)
+     resp = await post(f"https://batbin.me/api/v2/paste", data=text)
      code = resp["message"]
      return f"https://batbin.me/{code}"
