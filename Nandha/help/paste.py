4ec9ad9b19a22cf451aa4a0f82dda9c275812c3a
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-21 21:50:00 +0530

    Update paste.py

--- Nandha/help/paste.py
+++ Nandha/help/paste.py
@@ -1,8 +1,13 @@
-from Nandha.help.aiohttp import post
+from Nandha.help.aiohttp import post as bpost
 
+from requests import post as spost
 
-
-async def paste(text: str):
-     resp = await post(f"https://batbin.me/api/v2/paste", data=text)
+async def batbin(text):
+     resp = await bpost(f"https://batbin.me/api/v2/paste", data=text)
      code = resp["message"]
      return f"https://batbin.me/{code}"
+
+async def spacebin(text):
+    url = "https://spaceb.in/api/v1/documents/"
+    res = spost(url, data={"content": text, "extension": "txt"})
+    return f"https://spaceb.in/{res.json()['payload']['id']}"
