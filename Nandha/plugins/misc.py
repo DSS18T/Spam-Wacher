6e8fd3ccc8a6ceef0e18de589dbf636e7c93e220
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-21 22:01:41 +0530

    Update misc.py

--- Nandha/plugins/misc.py
+++ Nandha/plugins/misc.py
@@ -4,6 +4,7 @@ import config
 from Nandha import Nandha
 from pyrogram import filters
 from datetime import datetime as time
+from Nandha.help.paste import spacebin
 
 @Nandha.on_message(filters.command("ping",config.CMDS))
 async def ping(_, message):
@@ -11,3 +12,24 @@ async def ping(_, message):
       end = time.now()
       ping = (end - start).microseconds / 1000
       await message.reply(f"**PING**: `{ping}` ms")
+
+@Nandha.on_message(filters.command("paste",config.CMDS))
+async def paste(_, message):
+    reply = message.reply_to_message
+    chat_id = message.chat.id
+    if reply and not reply.text or not reply.caption or not reply.document:
+         return await message.reply("`please reply to (text/file) or give text to paste!`")
+    elif reply and reply.text or reply.caption:
+         text = reply.text or reply.caption
+    elif reply and reply.document:
+          async with aiofiles.open(doc, mode="r") as f:
+               text = await f.read()
+          os.remove(doc)
+    elif not reply and len(message.text.split()) <2:
+         return await message.reply("`please reply to (text/file) or give text to paste!`")
+    elif not reply and len(message.text.split()) >1:
+         text = message.text.split(None, 1)[1]
+    link = await spacebin(text)
+    await Nandha.send_message(chat_id,f"`{link}`",reply_to_message_id=message.id)
+    
+        
