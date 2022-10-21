61b0a20cdfe86aa5b28e4975591467fd8a06766d
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-21 11:11:56 +0530

    Update encodes.py

--- Nandha/plugins/encodes.py
+++ Nandha/plugins/encodes.py
@@ -9,7 +9,15 @@ from pyrogram import filters
 async def encodes(_, message):
       reply = message.reply_to_message
       chat_id = message.chat.id
-      if reply:
-            encodedBytes = base64.b64encode(data.encode("utf-8"))
-            encoded = str(encodedBytes, "utf-8")
-            await Nandha.send_message(chat_id, text=encoded, reply_to_message_id=message.id)
+
+      if reply and not reply.text:
+          return await message.reply("`reply to message text!`")
+      elif reply and reply.text:
+           data = reply.text
+      elif not reply and len(message.text.split()) <2:
+             return await message.reply("`reply to text or give me some text to encode!`")
+      elif not reply and len(message.text.split()) >1:
+            data = message.text.split(None,1)[1]
+      encodedBytes = base64.b64encode(data.encode("utf-8"))
+      encoded = str(encodedBytes, "utf-8")
+      await Nandha.send_message(chat_id, text=encoded, reply_to_message_id=message.id)
