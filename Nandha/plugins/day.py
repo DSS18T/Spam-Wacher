cbd4cf0a8c41619064c4f2d552888b893ecc3c86
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-21 19:02:47 +0530

    Update day.py

--- Nandha/plugins/day.py
+++ Nandha/plugins/day.py
@@ -8,19 +8,6 @@ import random
 from Nandha import Nandha
 from pyrogram import filters
 
-from datetime import datetime
-
-async def get_part_of_day(h):
-    return (
-        "morning"
-        if 5 <= h <= 11
-        else "afternoon"
-        if 12 <= h <= 17
-        else "evening"
-        if 18 <= h <= 22
-        else "night"
-    )
-
 
 GM_TEXT = [
 "Let your morning be the start of your new life. Do your best and forget about the rest. Good morning!",
@@ -48,37 +35,45 @@ GN_TEXT = [
 "Good night dear. Tomorrow, you are going to have a great day. Just make sure your body is prepared to take on the challenges of tomorrow. Sleep well!",
 "A new morning is waiting for you. Sleep well and sleep tight. Because the new day wants you to be fit and all charged up. Good night!",]
 
+GE_TEXT = [
+"Evenings allow you to forget the bitter worries of the day and get ready for the sweet dreams of night. Evenings give you relaxation from a stressful day. Good evening!",
+"A very good evening to you. Just wish that you have a good time. Let the fun begin with that super wide grin. Have a great time. Good evening!",
+"Stay happy and the situation will also turn happy, stay positive and keep smiling, and always stay in bliss. Good evening!",
+"Evenings are great not because it is the coolest time of the day, but because it lets you reflect on your whole day and forget your yesterday. Good evening!",
+"Sunsets are amazing, hope you are enjoying your evening perfectly!",
+"Good evening, hope you had a wonderful day and may you have a celebratory night!",
+"Take a look at the sunset and see the beautiful colors of life, good evening!",
+"Some days are going to be tough, some are going to be good but you have to keep your head strong and believe in yourself. Good evening!",
+"Every day brings unknown opportunities and challenges. But it ends with a peaceful evening. Hope you are enjoying this evening after a rewarding day. Good Evening dear friend!",
+"I miss your presence every evening. Our hot cup of coffee, long talks and game of chess. Miss you a lot, good evening.",
+"From morning till evening, life is full of momentous moments. Have a happy evening!",
+"The evening is a pause button of a hectic day. Enjoy the time and get ready for the another today. Good Evening.",
+]
 
+GA_TEXT = [
+"Good afternoon, enjoy this amazing afternoon. I cherish and love you so much.",
+"You mean the entire world to me, sweetheart; enjoy your afternoon.",
+"I miss you a lot, dear. I hope you are here with me this beautiful afternoon. Blessed afternoon.",
+"Even though distance has kept you away from me this afternoon, I hope you have a lovely afternoon. I love you.",
+"You are a God-given gem to me. Very rare, unique, and I will forever treasure you. Enjoy your afternoon, darling.",
+"I miss every moment we sit and share our thoughts and our feelings. Good afternoon my king.",
+"This afternoon sunshine keeps triggering the thoughts of having you. Your love towards me enlightens me and gives me a reason to yearn for tomorrow. Good afternoon handsome.",
+"How I hope you have a refreshing afternoon. Take a deep breath, and you will smell the love I have for you in the air. I give you my whole heart, love you.",
+"I can’t wait to have you in my arms this evening. Have a beautiful afternoon and come to me as soon as possible.",
+"My dear friend, like the water, evaporates in the sun; your gloomy days will soon disappear. Keep up the good work and give your best.",
+"There is nothing like coincidences or mistakes; everything that happens in our lives is a blessing. It is a stepping stone to a much longer journey. Enjoy your afternoon, my dear friend.",
+"How I wish you could spend the whole day with me, but your dreams are the priority for both of us. However, after you have completed it, I will always be waiting for you. Have a beautiful afternoon.",]
 
-@Nandha.on_message(group=20)
+@Nandha.on_message(group=30)
 async def day(_, message):
     part = await get_part_of_day(datetime.now().hour)
-    if message.text in ("good morning","good night","good afternoon"):
-           api = requests.get("https://api.waifu.pics/sfw/smile").json()
-           url = api["url"]
-           if "morning" in message.text:
-               quote = random.choice(GM_TEXT)
-           elif "night" in message.text:
-                 quote = random.choice(GN_TEXT)
-           elif "afternoon" in message.text:
-                 qoute = "good afternoon qoute"
-           elif "evening" in message.text:
-                 quote = "good evening quote"
-           day = datetime.now()
-           dayname = day.strftime("%A")
-           date = f"{day.day}-{day.month}-{day.year}"
-           mention = message.from_user.mention
-           await message.reply_animation(url,caption=f"""
-🤗 **Hi and good {part} 
-{mention}**, **we have nice quote for this {part}!**
-
-**quote**:
-{quote}
-
-**this is {dayname} and date 
-{date} my wish's for you have good day today!**
-""")
-
-
-
-
+    if message.text.lower() in ("good morning","good night","good afternoon","good evening"):
+           if "morning" in message.text.lower():
+                  await message.reply(text="*yee? {} good morning! and for you my quote 🥰*\n`{}`".format(message.from_user.mention, random.choice(GM_TEXT)))
+           elif "night" in message.text.lower():
+                  await message.reply(text="*yee? {} good night! and for you my quote 🥰*\n`{}`".format(message.from_user.mention, random.choice(GN_TEXT)))
+           elif "evening" in message.text.lower():
+                  await message.reply(text="*yee? {} good evening! and for you my quote 🥰*\n`{}`".format(message.from_user.mention, random.choice(GE_TEXT)))
+           elif "afternoon" in message.text.lower():
+                  await message.reply(text="*yee? {} good afternoon! and for you my quote 🥰*\n`{}`".format(message.from_user.mention, random.choice(GA_TEXT)))
+           
