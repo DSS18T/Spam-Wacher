9ba0b2a2dead5210895569db6fb563b599eb039f
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-21 21:02:40 +0530

    Update fun.py

--- Nandha/plugins/fun.py
+++ Nandha/plugins/fun.py
@@ -9,9 +9,10 @@ async def baka(_, message):
        reply = message.reply_to_message
        api = requests.get("https://nekos.best/api/v2/baka").json()
        url = api["results"][0]['url']
-       anime = api["results"][0]["anime_name"]
-       name = message.from_user.first_name
+       anime = api["results"][0]["anime_name"]     
        if reply:
+            name = reply.from_user.first_name
             await reply.reply_animation(url,caption="**• {}**\n**Baka! {}**".format(anime, name))
        else:
+           name = message.from_user.first_name
            await message.reply_animation(url,caption="**• {}**\n**Baka! {}**".format(anime, name))
