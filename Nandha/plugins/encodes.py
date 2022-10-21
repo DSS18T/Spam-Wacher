f347e13a00b86822382b409f8783fac65e3cc33b
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-21 11:33:04 +0530

    Update encodes.py

--- Nandha/plugins/encodes.py
+++ Nandha/plugins/encodes.py
@@ -5,7 +5,7 @@ from pyrogram import filters
 
 
 
-@Nandha.on_message(filters.command(["decode","encode"],config.CMDS))
+@Nandha.on_message(filters.command("encode",config.CMDS))
 async def encodes(_, message):
       reply = message.reply_to_message
       chat_id = message.chat.id
@@ -18,12 +18,25 @@ async def encodes(_, message):
              return await message.reply("`reply to text or give me some text to encode!`")
       elif not reply and len(message.text.split()) >1:
             data = message.text.split(None,1)[1]
-      if message.text.split()[0].startswith("en"):
-             encodedBytes = base64.b64encode(data.encode("utf-8"))
-             encoded = str(encodedBytes, "utf-8")
-             await Nandha.send_message(chat_id, text=encoded, reply_to_message_id=message.id)
-      elif message.text.split()[0].startswith("de"):
-             decodedbytes = base64.b64decode(data)
-             decoded = str(decodedbytes, "utf-8")
-             pass
-             await Nandha.send_message(chat_id, text=decoded, reply_to_message_id=message.id)
+      encodedBytes = base64.b64encode(data.encode("utf-8"))
+      encoded = str(encodedBytes, "utf-8")
+      await Nandha.send_message(chat_id, text=encoded, reply_to_message_id=message.id)
+      
+@Nandha.on_message(filters.command("decode",config.CMDS))
+async def decodes(_, message):
+      reply = message.reply_to_message
+      chat_id = message.chat.id
+
+      if reply and not reply.text:
+          return await message.reply("`reply to message text!`")
+      elif reply and reply.text:
+           data = reply.text
+      elif not reply and len(message.text.split()) <2:
+             return await message.reply("`reply to text or give me some text to encode!`")
+      elif not reply and len(message.text.split()) >1:
+            data = message.text.split(None,1)[1]
+      decodedbytes = base64.b64decode(data)
+      decoded = str(decodedbytes, "utf-8")
+      await Nandha.send_message(chat_id, text=decoded, reply_to_message_id=message.id)
+
+             
