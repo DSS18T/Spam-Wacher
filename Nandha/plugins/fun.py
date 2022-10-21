3dae74b6a5beec24d4e9ab7ff4f13d508f880c34
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-21 20:57:09 +0530

    Create fun.py

---
+++ Nandha/plugins/fun.py
@@ -0,0 +1,17 @@
+import requests
+from Nandha import Nandha
+from pyrogram import filters
+
+
+
+@Nandha.on_message(filters.regex("baka"))
+async def baka(_, message):
+       reply = message.reply_to_message
+       api = requests.get("https://nekos.best/api/v2/baka").json()
+       url = api["results"][0]['url']
+       anime = api["results"][0]["anime_name"]
+       name = message.from_user.first_name
+       if reply:
+            await reply.reply_animation(caption="**• {}**\n**Baka! {}**".format(anime, name))
+       else:
+           await message.reply_animation(caption="**• {}**\n**Baka! {}**".format(anime, name))
