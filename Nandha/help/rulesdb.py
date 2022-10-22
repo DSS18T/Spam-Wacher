cff9dec80d21db8e251a39abd392498fd59f91c4
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-22 16:01:28 +0530

    Update rulesdb.py

--- Nandha/help/rulesdb.py
+++ Nandha/help/rulesdb.py
@@ -5,7 +5,5 @@ rulesdb = mongodb.RULES
 
 
 def addrules(chat_id: int, text: str):
-    if chat_id in get_chat():
-      return
     RULES = {"_id": chat_id, "rules": text}
     return rulesdb.insert_one(RULES)
