ef024ee6667e19d8c9493668b2b60cdfe8f7e912
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-22 11:02:21 +0530

    Update Aibot.py

--- Nandha/plugins/Aibot.py
+++ Nandha/plugins/Aibot.py
@@ -56,21 +56,29 @@ async def chatbot(_, message):
 async def chatbot(_, query):
      chat_id = query.message.chat.id
      user_id = query.from_user.id
-     if (await is_admin(chat_id,user_id)) == False:
-          return await query.answer("Admins Only!", show_alert=True)
-     else:
+     if message.chat.type == enums.ChatType.PRIVATE:
           addchat(chat_id)
           await query.message.edit(f"`Successfully Connected ChatBot!`\nby **{query.from_user.first_name}**")
+     else:
+        if (await is_admin(chat_id,user_id)) == False:
+            return await query.answer("Admins Only!", show_alert=True)
+        else:
+            addchat(chat_id)
+            await query.message.edit(f"`Successfully Connected ChatBot!`\nby **{query.from_user.first_name}**")
      
 @Nandha.on_callback_query(filters.regex("disable_CB"))
 async def chatbot(_, query):
      chat_id = query.message.chat.id
      user_id = query.from_user.id
-     if (await is_admin(chat_id,user_id)) == False:
-          return await query.answer("Admins Only!", show_alert=True)
-     else:
+     if message.chat.type == enums.ChatType.PRIVATE:
           removechat(chat_id)
           await query.message.edit(f"`Successfully Disconnected ChatBot!`\nby **{query.from_user.first_name}**")
+     else:
+        if (await is_admin(chat_id,user_id)) == False:
+            return await query.answer("Admins Only!", show_alert=True)
+        else:
+            removechat(chat_id)
+            await query.message.edit(f"`Successfully Disconnected ChatBot!`\nby **{query.from_user.first_name}**")
      
 
     
