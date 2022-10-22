421750056a89aa7e5e7cf2e41edf0ed87c8af931
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-22 08:09:12 +0530

    Create chatbotdb.py

---
+++ Nandha/help/chatbotdb.py
@@ -0,0 +1,16 @@
+from Nandha import mongodb
+
+chatbotdb = mongodb.CHATBOT 
+
+def addchat(chat_id: int):
+    chatbotdb.insert_one({"chat_id": chat_id})
+
+def removechat(chat_id: int):
+    x = chatbotdb.find_one({"chat_id": chat_id})  
+    chatbotdb.delete_one(x)
+
+def is_chat(chat_id: int):
+     x = chatbotdb.find_one({"chat_id": chat_id}) 
+     if x:
+        return True
+     return False 
