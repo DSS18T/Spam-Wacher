36a150b0923b7fd8cdd51f7940a595b2e3969e2c
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-22 14:11:33 +0530

    Update misc.py

--- Nandha/plugins/misc.py
+++ Nandha/plugins/misc.py
@@ -32,7 +32,7 @@ async def ud(_, message):
       elif not reply and len(message.text.split()) >1:
             try:
               search = (
-                   if message.text.split()) <3:
+                   if len(message.text.split()) <3:
                        message.text.split(None,1)[1]
                    else:
                        message.text.split(None,1)[1].replace(" ","%20")
