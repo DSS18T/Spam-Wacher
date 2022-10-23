203d08bfcbde238fc9f9e4c0829891c0e633564b
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-23 23:38:31 +0530

    Update __init__.py

--- Nandha/__init__.py
+++ Nandha/__init__.py
@@ -11,7 +11,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[logging.FileHandler('logs.txt'),
               logging.StreamHandler()],
-    level=logging.DEBUG)
+    level=logging.INFO)
 
 LOGGER = logging.getLogger(__name__)
 
