d17c5d0c59f32fa49ef8ea4617ed1370826a75a8
Nandhasigma <115734626+Nandhasigma@users.noreply.github.com>
2022-10-23 09:04:24 +0530

    Update fun.py

--- Nandha/plugins/fun.py
+++ Nandha/plugins/fun.py
@@ -47,3 +47,12 @@ async def insult(_, message):
       except Exception as e:
           await message.reply(e)
 
+@Nandha.on_message(filters.command("riddle"))
+async def riddle(_, message):
+     riddle = requests.get("https://riddles-api.vercel.app/random").json()
+     question = riddle["riddle"]
+     answer = riddle["answer"]
+     msg = await message.reply(f"**• Riddle**:\n[ `{question}` ]\n\n[ `The Answer will show automaticly 10seconds after tell me your guess's!` ]")
+     await asyncio.sleep(10)
+     await msg.edit(f"**• Riddle**:\n[ `{question}` ]\n\n• **Answer**: [ `{answer}` ]")
+     
