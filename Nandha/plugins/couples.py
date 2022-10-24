61cfebfae5325008393f229b0dda60b3ae449fdb
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-24 12:42:03 +0530

    Update couples.py

--- Nandha/plugins/couples.py
+++ Nandha/plugins/couples.py
@@ -6,7 +6,7 @@ from pyrogram import enums
 
 from Nandha import Nandha
 
-@Nandha.on_message(filters.command("config"))
+@Nandha.on_message(filters.command("couples",config))
 async def couples(_, message):
      chat_id = message.chat.id
      if message.chat.type == enums.ChatType.PRIVATE:
@@ -16,18 +16,19 @@ async def couples(_, message):
         async for member in Nandha.get_chat_members(chat_id):
                if not member.user.is_bot:
                      couples.append(member.user.id)
-               if len(couples) <2:
-                    return await message.reply("`Not enough Members!`")
-               men = random.choice(couples)
-               women = random.choice(couples)
-               while men == women:
-                    men = random.choice(couples)
-               men_m = (await Nandha.get_users(men)).mention
-               women_m = (await Nandha.get_users(women)).mention
-               await message.reply(f"""**New Couples Arrived!**
-Men: **{men_m}**
-women: **{women_m}**
+        if len(couples) <2:
+             return await message.reply("`Not enough Members!`")
+        men = random.choice(couples)
+        women = random.choice(couples)
+        while men == women:
+            men = random.choice(couples)
+        men_m = (await Nandha.get_users(men)).mention
+        women_m = (await Nandha.get_users(women)).mention
+        await message.reply(f"""**New Couples Arrived!**
 
-**we all wish you happy marrie life 🤭🤭!**
+**Men**: **{men_m}**
+**women**: **{women_m}**
+
+**we all wish you happy marrie life 🤭🤭😂❤️!**
 """)
                
