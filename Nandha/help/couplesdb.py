7b7e44538e4f3361940aa549501e5dbfd5ac62bf
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-24 15:33:52 +0530

    Update couplesdb.py

--- Nandha/help/couplesdb.py
+++ Nandha/help/couplesdb.py
@@ -12,4 +12,14 @@ def get_couple(chat_id: int):
 def save_couple(chat_id: int, date, couples):
       COUPLES = {"_id":chat_id, "date":date, "couple": couples}
       coupledb.insert_one(COUPLES)
-      
+
+
+
+def check_couple(chat_id: int, date, couple):
+     couples = couple db.find_one({"_id": chat_id})
+     if couple["date"] == date:
+         return get_couple(chat_id)
+     else:
+         coupledb.update_one({"_id": chat_id},{"$set":{"couple":couple}})
+
+             
