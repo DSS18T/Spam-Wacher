9bf842910d3580c27c9faf99878320ac37ad7929
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-25 22:50:45 +0530

    Update bans.py

--- Nandha/plugins/bans.py
+++ Nandha/plugins/bans.py
@@ -39,13 +39,57 @@ async def bans(_, message):
                        return await message.reply_text("`The User Is Admin! I can't ban!`")
                 if re.search("s", message.text.split()[0]):
                       msg = await Nandha.ban_chat_member(chat_id, ban_id)
-                      await msg.delete()
+                      if msg.service:
+                          await msg.delete()
+                          await message.delete()
                       await message.delete()
-                      pass
                 else:
                     await Nandha.ban_chat_member(chat_id, ban_id)
                     await message.reply_animation(url,caption=f"The Bitch As Dust!\n • `{ban_id}`\n\nFollowing Reason:\n`{reason}`",
                     reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Unban", callback_data=f"unban_btn:{ban_id}")]]))          
+      except Exception as e:
+         await message.reply_text(e)
+
+@Nandha.on_message(filters.command(["skick","kick"],config.CMDS))
+async def kicks(_, message):
+      user_id = int(message.from_user.id)
+      chat_id = int(message.chat.id)
+      reply = message.reply_to_message
+      api = requests.get("https://api.waifu.pics/sfw/kick").json()
+      url = api["url"]
+      try:
+          if (await can_ban_members(chat_id,user_id)) == True or message.from_user.id in config.DEVS:   
+                if not reply and len(message.command) >2:
+                    kick_id = (await Nandha.get_users(message.text.split(" ")[1])).id
+                    reason = message.text.split(None, 2)[2]
+                elif not reply and len(message.command) == 2:
+                    kick_id = (await Nandha.get_users(message.text.split(" ")[1])).id
+                    reason = "No Reason Provide!"
+                elif reply and len(message.command) >1:
+                    kick_id = reply.from_user.id
+                    reason = message.text.split(None, 1)[1]        
+                elif reply and len(message.command) <2:
+                     kick_id = reply.from_user.id
+                     reason = "No Reason Provide!"
+                if (await is_admin(chat_id, config.BOT_ID)) == False:
+                      return await message.reply_text("`Make you sure I'm Admin!`")
+                elif kick_id == config.BOT_ID:
+                       return await message.reply_text("`I can't ban myself!`")
+                elif kick_id in config.DEVS:
+                       return await message.reply_text("`I can't do against my owner!`")
+                elif (await is_admin(chat_id, ban_id)) == True:
+                       return await message.reply_text("`The User Is Admin! I can't ban!`")
+                if re.search("s", message.text.split()[0]):
+                      msg = await Nandha.ban_chat_member(chat_id, kick_id)
+                      await Nandha.unban_chat_member(chat_id, kick_id)
+                      if msg.service:
+                          await msg.delete()
+                          await message.delete()
+                      await message.delete()
+                else:
+                    await Nandha.ban_chat_member(chat_id, kick_id)
+                    await Nandha.unban_chat_member(chat_id, kick_id)
+                    await message.reply_animation(url,caption=f"KickOut As Dust!\n • `{ban_id}`\n\nFollowing Reason:\n`{reason}`")          
       except Exception as e:
          await message.reply_text(e)
        
