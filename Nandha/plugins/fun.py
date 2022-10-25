bcc550282bf77f8ebaabdb73e2adcaf97744b560
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-26 00:02:21 +0530

    Update fun.py

--- Nandha/plugins/fun.py
+++ Nandha/plugins/fun.py
@@ -64,7 +64,7 @@ async def quote(_, m):
     string = api["text"]
     author = api["author"]
     await m.reply(
-        "**Quotes**: `{string}`\n\n"
-        "**Author**: `{author}`")
+        f"**Quotes**: `{string}`\n\n"
+        f"**Author**: `{author}`")
         
 
