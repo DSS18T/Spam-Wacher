6e5f64b0751d12d6ebbeabdfb917e5ce2747b4a1
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-26 13:19:00 +0530

    Update misc.py

--- Nandha/plugins/misc.py
+++ Nandha/plugins/misc.py
@@ -75,20 +75,20 @@ async def ud(_, message):
             results = requests.get("https://api.urbandictionary.com/v0/define?term="+search).json()
             text = f'**⚠️ Warning: Urban Dictionary does not always provide accurate descriptions**:\n\n**• Result for**: `[{query}]`\n\n**• Result**:\n`{results["list"][0]["definition"]}`\n\n• **Example**:\n`{results["list"][0]["example"]}`'
             await Nandha.send_message(message.chat.id,text=text,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🧠 Google it",url="https://www.google.com/search?q=define%20"+search),]]),reply_to_message_id=message.id)
-         except IndexError:
+         except Exception as e:
               await message.reply("`No Result Found!`")
       elif not reply and len(message.text.split()) >1:
             try:
               query = message.text.split(None,1)[1]
               search = (
-                   message.text.split(None,1)[1]
+                   message.text.split()[1]
                    if len(message.text.split()) <3
                    else message.text.split(None,1)[1].replace(" ","%20")
               )
               results = requests.get("https://api.urbandictionary.com/v0/define?term="+search).json()
               text = f'**⚠️ Warning: Urban Dictionary does not always provide accurate descriptions**:\n\n**• Result for**: `[{query}]`\n\n**• Result**:\n`{results["list"][0]["definition"]}`\n\n• **Example**:\n`{results["list"][0]["example"]}`'
               await Nandha.send_message(message.chat.id,text=text, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🧠 Google it",url="https://www.google.com/search?q=define%20"+search),]]),reply_to_message_id=message.id)
-            except IndexError:
+            except Exception as e:
               await message.reply("`No Result Found!`")  
 
 @Nandha.on_message(filters.command("paste",config.CMDS))
