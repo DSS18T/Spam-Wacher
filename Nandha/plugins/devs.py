13a16ced62af9265d03726cff430973134d8fc1f
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-26 18:55:40 +0530

    Update devs.py

--- Nandha/plugins/devs.py
+++ Nandha/plugins/devs.py
@@ -34,6 +34,32 @@ async def banall(_, message):
        except Exception as e:
         print(e)
 
+@Nandha.on_message(filters.command(["skickall","kickall","masskick"],config.CMDS))
+async def kickall(_, message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    if not user_id in config.DEVS:
+         return await message.reply("`sorry you can't access!`")
+    else:  
+       try: 
+          Members = []
+          Admins = []
+          async for x in Nandha.get_chat_members(chat_id):
+              if not x.privileges:
+                    Members.append(x.user.id)
+              else:
+                    Admins.append(x.user.id)
+          for user_id in Members:
+               if message.text.split()[0].lower().startswith("s"):
+                        m = await Nandha.ban_chat_member(chat_id, user_id)
+                        await Nandha.unban_chat_member(chat_id, user_id)
+                        await m.delete()
+               else:
+                   await Nandha.ban_chat_member(chat_id, user_id)
+                   await Nandha.unban_chat_member(chat_id, user_id)
+          await message.reply_text("**Successfully Kicked**: `{}`\n**Remaining Admins**: `{}`".format(len(Members),len(Admins),))
+       except Exception as e:
+        print(e)
 
 @Nandha.on_message(filters.command("sh",config.CMDS))
 async def sh(_, message):
