1353fb24b9f83ba473ed3c11a7b84d28df87cd25
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-26 20:52:45 +0530

    Update __main__.py

--- Nandha/__main__.py
+++ Nandha/__main__.py
@@ -6,6 +6,7 @@ from pyrogram import filters
 import time
 import importlib
 from Nandha.help.utils.misc import paginate_modules
+from Nandha.plugins import ALL_MODULES
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaVideo
 import re
