876a3b2aee5cf56a4c40b052f8699d0f0d3e7f82
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-26 23:13:57 +0530

    Update __main__.py

--- Nandha/__main__.py
+++ Nandha/__main__.py
@@ -13,6 +13,23 @@ import re
 
 HELPABLE = {}
 
+def get_helps():
+    global HELPABLE
+    for module in ALL_MODULES:
+        imported_module = importlib.import_module("Nandha.plugins." + module)
+        if (
+            hasattr(imported_module, "__MODULE__")
+            and imported_module.__MODULE__
+        ):
+            imported_module.__MODULE__ = imported_module.__MODULE__
+            if (
+                hasattr(imported_module, "__HELP__")
+                and imported_module.__HELP__
+            ):
+                HELPABLE[
+                    imported_module.__MODULE__.replace(" ", "_").lower()
+                ] = imported_module
+
 
 get_helps()
 
