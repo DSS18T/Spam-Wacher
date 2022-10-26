a3e35ab31766c76c585cf62c42ee8f90b4f03295
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-27 00:29:14 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,7 +1,7 @@
 
 START_TEXT = """
-read the help menu as well 
-then you know. how to use the commands! And I'm SpamWatcher (I well promise i can protect your group) scam and pornography contents.
+read the help menu as well
+then you know. how to use the commands! And I'm SpamWatcher `[I well promise i can protect your group]` scam and pornography contents.
 """
 
 HELP_TEXT = """
