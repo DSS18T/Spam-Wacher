9192b35390e382b317bea6337fc1d3551e6c9e41
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-26 15:19:12 +0530

    Update misc.py

--- Nandha/plugins/misc.py
+++ Nandha/plugins/misc.py
@@ -65,6 +65,19 @@ async def ping(_, message):
 @Nandha.on_message(filters.command("ud",config.CMDS))
 async def ud(_, message):
       reply = message.reply_to_message
+      if reply and len(message.text.split()) >1:
+            try: 
+              query = message.text.split(None,1)[1].lower()
+              search = (
+                   message.text.split()[1]
+                   if len(message.text.split()) <3
+                   else message.text.split(None,1)[1].replace(" ","%20")
+              )
+              results = requests.get("https://api.urbandictionary.com/v0/define?term="+search).json()
+              text = f'**⚠️ Warning: Urban Dictionary does not always provide accurate descriptions**:\n\n**• Result for**: `[{query}]`\n\n**• Result**:\n`{results["list"][0]["definition"]}`\n\n• **Example**:\n`{results["list"][0]["example"]}`'
+              await Nandha.send_message(message.chat.id,text=text, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🧠 Google it",url="https://www.google.com/search?q=define%20{}".format(search)),]]),reply_to_message_id=reply.id)
+            except Exception as e:
+              await message.reply("No Results Found!")  
       if not reply and len(message.text.split()) >1:
             try: 
               query = message.text.split(None,1)[1].lower()
