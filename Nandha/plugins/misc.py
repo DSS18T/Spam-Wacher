bd67a476cfcf3a6ff88444a72f48c67697ed773d
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-26 14:30:49 +0530

    Update misc.py

--- Nandha/plugins/misc.py
+++ Nandha/plugins/misc.py
@@ -68,10 +68,11 @@ async def ud(_, message):
       if reply and reply.text:
          try:
             query = reply.text.lower()
-            if len(reply.text.split()) <2:
-                 search = reply.text.split()[0]
-            else:
-                 search = reply.text.replace(" ", "%20")
+            search = (
+                   message.text.split(None,0)[0]
+                   if len(message.text.split()) <2
+                   else message.text.split(None,1)[1].replace(" ","%20")
+              )
             results = requests.get("https://api.urbandictionary.com/v0/define?term="+search).json()
             text = f'**⚠️ Warning: Urban Dictionary does not always provide accurate descriptions**:\n\n**• Result for**: `[{query}]`\n\n**• Result**:\n`{results["list"][0]["definition"]}`\n\n• **Example**:\n`{results["list"][0]["example"]}`'
             await Nandha.send_message(message.chat.id,text=text,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🧠 Google it",url="https://www.google.com/search?q=define%20"+search),]]),reply_to_message_id=message.id)
@@ -80,10 +81,11 @@ async def ud(_, message):
       elif not reply and len(message.text.split()) >1:
             try:
               query = message.text.split(None,1)[1].lower()
-              if len(message.text.split()) < 3:
-                   search = message.text.split()[1]
-              else:
-                   search = message.text.replace(" ", "%20")
+              search = (
+                   message.text.split(None,1)[1]
+                   if len(message.text.split()) <3
+                   else message.text.split(None,1)[1].replace(" ","%20")
+              )
               results = requests.get("https://api.urbandictionary.com/v0/define?term="+search).json()
               text = f'**⚠️ Warning: Urban Dictionary does not always provide accurate descriptions**:\n\n**• Result for**: `[{query}]`\n\n**• Result**:\n`{results["list"][0]["definition"]}`\n\n• **Example**:\n`{results["list"][0]["example"]}`'
               await Nandha.send_message(message.chat.id,text=text, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🧠 Google it",url="https://www.google.com/search?q=define%20"+search),]]),reply_to_message_id=message.id)
