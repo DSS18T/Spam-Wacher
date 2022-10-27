2adeaed1989bc9cc62780ebfe32e3d14783a86b4
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-27 17:50:10 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,4 +1,4 @@
 
 START_TEXT = "Read the help menu as well then you know. how to use the commands! And I'm SpamWatcher `[I well promise i can protect your group]` scam and pornography contents."
 
-HELP_TEXT = "I have a some fummy and useful commandsyou can also use on your groups. and below myavailable modules that can help you!"
+HELP_TEXT = "I have a some fummy and useful commands you can also use on your groups. and below my available modules that can help you!"
