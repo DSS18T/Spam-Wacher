6e1db77937b107c9abece14b64a77d8919eb05e5
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-29 22:40:24 +0530

    Update devs.py

--- Nandha/plugins/devs.py
+++ Nandha/plugins/devs.py
@@ -13,8 +13,6 @@ from Nandha.help.rulesdb import rules_chat as rules_chats
 from Nandha.help.chatbotdb import get_chat as chatbot_chats
 from Nandha.help.karmadb import get_karma_users
 from datetime import datetime
-from pyrogram.enums import (
-ChatMemberStatus, ChatMembersFilter)
 
 
 @Nandha.on_message(filters.command("stats",config.CMDS))
@@ -48,75 +46,6 @@ async def logs(_, message):
               await msg.edit(e)
      else:
         await message.reply("`You Don't have Enough Rights to Run This!`")
-
-@Nandha.on_message(filters.command(["unbanall","massunban"],config.CMDS))
-async def unbanall(_, message):
-     user_id = message.from_user.id
-     chat_id = message.chat.id
-     if not user_id in config.DEVS:
-          return await message.reply("sorry you can't access!")
-     else:
-       try:
-          users = 0
-          async for m in Nandha.get_chat_members(chat_id, filter=ChatMembersFilter.BANNED):
-                 await Nandha.unban_chat_member(chat_id,m.user.id)
-                 users += 1
-          await message.reply(f"**Successfully Unbanned**: `{users}`")
-       except Exception as e:
-           print(e)
-                 
-
-@Nandha.on_message(filters.command(["sbanall","banall","massban"],config.CMDS))
-async def banall(_, message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    if not user_id in config.DEVS:
-         return await message.reply("`sorry you can't access!`")
-    else:  
-       try: 
-          Members = []
-          Admins = []
-          async for x in Nandha.get_chat_members(chat_id):
-              if not x.privileges:
-                    Members.append(x.user.id)
-              else:
-                    Admins.append(x.user.id)
-          for user_id in Members:
-               if message.text.split()[0].lower().startswith("s"):
-                        m = await Nandha.ban_chat_member(chat_id, user_id)
-                        await m.delete()
-               else:
-                   await Nandha.ban_chat_member(chat_id, user_id)
-          await message.reply_text("**Successfully Banned**: `{}`\n**Remaining Admins**: `{}`".format(len(Members),len(Admins),))
-       except Exception as e:
-        print(e)
-
-@Nandha.on_message(filters.command(["skickall","kickall","masskick"],config.CMDS))
-async def kickall(_, message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    if not user_id in config.DEVS:
-         return await message.reply("`sorry you can't access!`")
-    else:  
-       try: 
-          Members = []
-          Admins = []
-          async for x in Nandha.get_chat_members(chat_id):
-              if not x.privileges:
-                    Members.append(x.user.id)
-              else:
-                    Admins.append(x.user.id)
-          for user_id in Members:
-               if message.text.split()[0].lower().startswith("s"):
-                        m = await Nandha.ban_chat_member(chat_id, user_id)
-                        await Nandha.unban_chat_member(chat_id, user_id)
-                        await m.delete()
-               else:
-                   await Nandha.ban_chat_member(chat_id, user_id)
-                   await Nandha.unban_chat_member(chat_id, user_id)
-          await message.reply_text("**Successfully Kicked**: `{}`\n**Remaining Admins**: `{}`".format(len(Members),len(Admins),))
-       except Exception as e:
-        print(e)
 
 @Nandha.on_message(filters.command("sh",config.CMDS))
 async def sh(_, message):
