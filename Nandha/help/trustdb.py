ea4741c5fae03c4971485d6c3ad752ff5e292934
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-29 12:20:57 +0530

    Update trustdb.py

--- Nandha/help/trustdb.py
+++ Nandha/help/trustdb.py
@@ -27,5 +27,5 @@ def add_trust(user_id: int):
           x = db.find_one({"_id": user_id})
           trust = int(x["trust"])+1
           db.update_one({"_id": user_id},{"$set":{"trust":trust}})
-     else:  db.insert_one({"_id": user_id,"user_id": user_id,"trust": 1})
+     else:  db.insert_one({"_id": user_id,"user_id": user_id,"trust": "1"})
        
