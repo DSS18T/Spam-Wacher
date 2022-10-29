53521c6e9f2adbabe9815346669d9185c3ca3f6d
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-29 13:52:40 +0530

    Update trust.py

--- Nandha/plugins/trust.py
+++ Nandha/plugins/trust.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 from pyrogram import enums
 from Nandha.help.trustdb import *
 
-TRUST = "trust|thank|pro|thx"
+TRUST = "thanks|thx"
 
 @Nandha.on_message(filters.regex(TRUST))
 async def trust(_, message):
@@ -17,5 +17,23 @@ async def trust(_, message):
      else: add_trust(user_id)
      trust = get_trust(user_id)
      await reply.reply(
-        f"**TRUST for {reply.from_user.first_name}**:\n\n"
+        f"**Trust for {reply.from_user.first_name}**:\n\n"
         f"**Trust increased**: `{trust}`")
+
+UNTRUST = "please|wtf|sed"
+
+@Nandha.on_message(filters.regex(UNTRUST))
+async def untrust(_, message):
+     reply = message.reply_to_message
+     if not reply:
+        return
+     user_id = reply.from_user.id
+     if message.from_user.id == user_id:
+          return 
+     elif not message.from_user.id in get_trust_users():
+          return
+     else: remove_trust(user_id)
+     trust = get_trust(user_id)
+     await reply.reply(
+        f"**Trust for {reply.from_user.first_name}**:\n\n"
+        f"**Trust Decreased**: `{trust}`")
