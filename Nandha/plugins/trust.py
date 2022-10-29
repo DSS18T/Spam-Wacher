545a8588750a92d5934d6b2974427912bd6ff81f
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-29 11:55:11 +0530

    Create trust.py

---
+++ Nandha/plugins/trust.py
@@ -0,0 +1,21 @@
+
+from Nandha import Nandha
+from pyrogram import filters
+from pyrogram import enums
+from Nandha.help.trustdb import *
+
+TRUST = "+|trust|thanks|pro"
+
+@Nandha.on_message(filters.regex(TRUST))
+async def trust(_, message):
+     reply = message.reply_to_message
+     if not reply:
+        return
+     user_id = reply.from_user.id
+     if message.from_user.id == user_id:
+          return 
+     else: add_trust(user_id)
+     trust = get_trust(user_id)
+     await message.reply(
+        "**TRUST**:\n"
+        f"**increased**: `{trust}`")
