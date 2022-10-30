cf03f2cadaa29831ee6ff3ac11d89d8cce4a7bec
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-30 16:57:27 +0530

    Update admin.py

--- Nandha/help/admin.py
+++ Nandha/help/admin.py
@@ -1,6 +1,16 @@
 
 from Nandha import Nandha
 
+from pyrogram import enums
+
+
+async def is_owner(chat_id: int, user_id: int):
+    async for x in Nandha.get_chat_members(chat_id):
+        if x.status == enums.ChatMemberStatus.OWNER:
+             if x.user.id == user_id:
+                 return True
+             else: return Flase
+
 async def is_admin(chat_id: int, user_id: int):
      admin = await Nandha.get_chat_member(chat_id, user_id)
      if admin.privileges:
