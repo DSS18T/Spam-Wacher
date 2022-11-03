7cc800f1457e58b2a14ba4b39bcce423114f066f
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-03 16:50:09 +0530

    Update warnsdb.py

--- Nandha/help/warnsdb.py
+++ Nandha/help/warnsdb.py
@@ -16,3 +16,12 @@ def add_warn(user_id: int, reason):
     warns = int(x["warn"])+1
     db.update_one({"user_id":user_id},{"$set":{"warn":warns,f"reason {warns}":reason}})
     
+def remove_warn(user_id: int):
+      x = db.find_one({"user_id":user_id})
+      db.delete_one(x)
+
+def is_warns(user_id: int):
+     if not user_id in warn_users():
+         return 0
+     x = db.find_one({"user_id":user_id})
+     return x["warn"]
