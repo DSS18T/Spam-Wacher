409028fcd8327d7cafd609a09197f68b23acb970
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-03 15:35:09 +0530

    Update warnsdb.py

--- Nandha/help/warnsdb.py
+++ Nandha/help/warnsdb.py
@@ -3,6 +3,12 @@ from Nandha import mongodb
 
 db = mongodb.WARNNING
 
+def warn_users():
+    warn_users = []
+    for x in db.find():
+       append(x["user_id"])
+    return warn_users
+
 def add_warn(user_id: int, reason):
     if not user_id in warn_users():
         db.insert_one({"user_id":user_id, "warn":1, "reason": reason})
