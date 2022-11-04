7eeeebea8c8d14bd05fe09eb7e2ac0b9f06f85e6
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-04 15:03:52 +0530

    Create carbon.py

--- Nandha/plugins/carbon.py
+++ Nandha/plugins/carbon.py
@@ -18,8 +18,8 @@ async def make_carbon(code):
 async def make_carbon_image(_, message):
       reply = message.reply_to_message
       if reply:
-           carbon = make_carbon(reply.text)
+           carbon = await make_carbon(reply.text)
       elif not reply and len(message.text.split()) >1:
-           carbon = make_carbon(message.text.split(None,1)[1])
+           carbon = await make_carbon(message.text.split(None,1)[1])
       else: return await message.reply("`Reply to text or give a text to make carbon!`")
       await message.reply_photo(carbon)
