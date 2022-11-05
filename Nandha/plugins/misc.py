ebba1f18da1b68079683b13fcd7e19ff666cd61c
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-05 16:46:33 +0530

    Update misc.py

--- Nandha/plugins/misc.py
+++ Nandha/plugins/misc.py
@@ -18,8 +18,12 @@ is_downloading = False
 @Nandha.on_message(filters.command("gt",config.CMDS))
 async def google_it(_, message):
        file_id = "CAACAgUAAx0CXss_8QABB0iVY2ZDrB4YHzW6u1xRqKLuUX7b6sEAAhUAA-VDzTc4Ts7oOpk4nx4E"
-       await message.reply_sticker(sticker=file_id,
+       if message.reply_to_message:
+            await message.reply_to_message.message.reply_sticker(sticker=file_id,
            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔎 Google",url="https://www.google.com/search?")]]))
+       else: await message.reply_sticker(sticker=file_id,
+           reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔎 Google",url="https://www.google.com/search?")]]))
+
 
 @Nandha.on_message(filters.command("img",config.CMDS))
 async def image(_, message):
