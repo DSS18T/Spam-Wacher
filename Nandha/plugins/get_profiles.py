803abd870ca1ae5e162481e36add419becbf148f
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-05 05:32:47 +0530

    Create get_profiles.py

---
+++ Nandha/plugins/get_profiles.py
@@ -0,0 +1,16 @@
+import config
+from Nandha import Nandha
+from pyrogram import filters
+from pyrogram.types import InputMediaPhoto
+
+
+@Nandha.on_message(filters.command("pfp",config.CMDS))
+async def user_profiles(_, message):
+       user_id = message.from_user.id
+       x = await Nandha.get_chat_photos_count(user_id)
+       async for photo in Nandha.get_chat_photos(user_id):
+              file = photo.file_id
+       await Nandha.send_media_group(message.chat.id,[
+       for x in file:
+            InputMediaPhoto(file_id)
+        ])
