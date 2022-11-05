0157775eb6f96c59a6240b664a97d7e1a0a934e7
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-05 07:43:29 +0530

    Update get_profiles.py

--- Nandha/plugins/get_profiles.py
+++ Nandha/plugins/get_profiles.py
@@ -2,15 +2,13 @@ import config
 import asyncio
 from Nandha import Nandha
 from pyrogram import filters
-from pyrogram.types import InputMediaPhoto
-
 
 @Nandha.on_message(filters.command("pfp",config.CMDS))
 async def user_profiles(_, message):
        user_id = message.from_user.id
        reply = message.reply_to_message
        try:
-          if reply: uid = reply.from_user.id; not reply: uid = user_id:
+          if reply: uid = reply.from_user.id; elif not reply: uid = user_id:
           msg = await message.reply_text("`Check My Private I'm Sending Photos You Requested!`")
           async for photo in Nandha.get_chat_photos(uid):
                await Nandha.send_photo(user_id, photo.file_id)
