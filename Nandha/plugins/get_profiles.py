d79b1cea9156dfdaabd980c126246905bf26fecd
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-05 07:46:28 +0530

    Update get_profiles.py

--- Nandha/plugins/get_profiles.py
+++ Nandha/plugins/get_profiles.py
@@ -8,7 +8,8 @@ async def user_profiles(_, message):
        user_id = message.from_user.id
        reply = message.reply_to_message
        try:
-          if reply: uid = reply.from_user.id elif not reply: uid = user_id:
+          if reply: uid = reply.from_user.id 
+          elif not reply: uid = user_id:
           msg = await message.reply_text("`Check My Private I'm Sending Photos You Requested!`")
           async for photo in Nandha.get_chat_photos(uid):
                await Nandha.send_photo(user_id, photo.file_id)
