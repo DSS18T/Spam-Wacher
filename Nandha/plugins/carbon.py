5801ac0aaf8493bff1ef38049d6efe8c0c744e0f
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-06 13:51:36 +0530

    Update carbon.py

--- Nandha/plugins/carbon.py
+++ Nandha/plugins/carbon.py
@@ -1,3 +1,5 @@
+import os
+import cv2
 import config
 from Nandha import (
 Nandha, session )
@@ -26,3 +28,44 @@ async def make_carbon_image(_, message):
       carbon = await make_carbon(text)
       await message.reply_photo(carbon)
       await msg.delete()
+
+@Nandha.on_message(filters.command("bw",config.CMDS))
+async def black_white(Nandha: client, message):
+    try:
+        userid = str(message.chat.id)
+        if not os.path.isdir(f"./DOWNLOADS/{userid}"):
+            os.makedirs(f"./DOWNLOADS/{userid}")
+        download_location = "./DOWNLOADS" + "/" + userid + "/" + userid + ".jpg"
+        edit_img_loc = "./DOWNLOADS" + "/" + userid + "/" + "black_white.jpg"
+        if not message.reply_to_message.empty:
+            msg = await message.reply_to_message.reply_text(
+                "Downloading image", quote=True
+            )
+            a = await client.download_media(
+                message=message.reply_to_message, file_name=download_location
+            )
+            await msg.edit("Processing Image...")
+            image_file = cv2.imread(a)
+            grayImage = cv2.cvtColor(image_file, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite(edit_img_loc, grayImage)
+            await message.reply_chat_action("upload_photo")
+            await message.reply_to_message.reply_photo(edit_img_loc, quote=True)
+            await msg.delete()
+        else:
+            await message.reply_text("Why did you delete that??")
+        try:
+            shutil.rmtree(f"./DOWNLOADS/{userid}")
+        except Exception:
+            pass
+    except Exception as e:
+        print("black_white-error - " + str(e))
+        if "USER_IS_BLOCKED" in str(e):
+            return
+        else:
+            try:
+                await message.reply_to_message.reply_text(
+                    "Something went wrong!", quote=True
+                )
+            except Exception:
+                return
+
