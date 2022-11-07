fe8d4000b0811f7e2294a06f6f20ebda3235d0fa
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-08 01:23:11 +0530

    Update image.py

--- Nandha/plugins/image.py
+++ Nandha/plugins/image.py
@@ -21,9 +21,12 @@ async def google_image(_, message):
      kk.append(c)
      image = []
      msg = await message.reply("`Processing....`")
-     for x in kk:
-         image.append(InputMediaPhoto(media=f"/app/gg_images/{x}"))
-     await Nandha.send_group_media(message.chat.id, media=image)    
+     try:
+       for x in kk:
+           image.append(InputMediaPhoto(media=f"/app/gg_images/{x}"))
+        await Nandha.send_media_group(message.chat.id, media=image)    
+        await msg.delete()
+     except Exception as e: return await message.reply(e)
      await msg.delete()
      os.remove(mm)
    
