686ed01828d0178415c385272db3a8a589b6e4d5
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-13 14:52:43 +0530

    Update broadcast.py

--- Nandha/plugins/broadcast.py
+++ Nandha/plugins/broadcast.py
@@ -8,16 +8,14 @@ from Nandha.help.usersdb import *
 
 @Nandha.on_message(filters.user(config.OWNER_ID) & filters.command("groupcast",config.CMDS))
 async def global_cast(_, message):
-      list = get_chats()
       reply = message.reply_to_message
-      chat = message.chat
-      done = 0
-      fail = 0
+      chat = message.chat  
       if not reply: return await message.reply("Reply to Message to Forward My all Groups")
       success = "**Globally Cast**:\n**Success**: `{}`\n**Failed**: `{}`"
+      list = get_chats(); done = 0; fail = 0
       for chat_id in list:
           try:
-             await Nandha.forward_messages(chat.id, chat_id, reply.id)
+             await Nandha.forward_messages(chat_id, chat.id, reply.id)
              done +=+1
              await asyncio.sleep(3)
           except: fail +=+1      
