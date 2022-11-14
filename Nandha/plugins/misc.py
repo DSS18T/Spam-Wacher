8347905cb9f80fe7e416a5154e629e60c757870c
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-14 22:56:43 +0530

    Update misc.py

--- Nandha/plugins/misc.py
+++ Nandha/plugins/misc.py
@@ -22,7 +22,7 @@ async def echo(_, message):
      chat_id = message.chat.id
      if reply: return await reply.copy(chat_id, reply_to_message_id=reply.id)
      elif len(message.text.split()) >1: return await message.reply_text(message.text.split(None,1)[1])
-     else: return await message.reply_text("What should I was echo?")
+     else: return await message.reply_text("What should I echo?")
 
 
 
