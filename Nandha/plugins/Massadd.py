d3204a7d2d2f6dd0bf709a443414bb87d2c0f5c5
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-18 21:40:55 +0530

    Update Massadd.py

--- Nandha/plugins/Massadd.py
+++ Nandha/plugins/Massadd.py
@@ -9,8 +9,9 @@ async def mass_add(_, message):
        chat = message.chat
        try: from_chat_id = message.text.split()[1]
        except: return await message.reply("/add chat_id")
-       members = []
+       done = 0
        async for m in UB.get_chat_members(from_chat_id):
-            members.append(m.user.id)
-       try: await UB.add_chat_members(chat.id, members)
-       except: pass
+          try: await UB.add_chat_members(chat.id, members); done +=+1
+          except: pass
+       await message.reply("done! {}".format(done))
+       
