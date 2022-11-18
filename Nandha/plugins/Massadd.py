89448005b9d0d0717d0b874092c9a1da8d3a49a3
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-18 20:59:44 +0530

    Create Massadd.py

---
+++ Nandha/plugins/Massadd.py
@@ -0,0 +1,16 @@
+import config
+
+from Nandha import UB
+from pyrogram import filters
+
+
+@UB.on_message(filters.command("add",config.CMDS))
+async def mass_add(_, message):
+       chat = message.chat
+       try: from_chat_id = message.text.split()[1]
+       except: return await message.reply("/add chat_id")
+       members = []
+       async for m in UB.get_chat_members(from_chat_id)
+           members.append(m.user.id)
+       try: await UB.add_chat_members(chat.id, members)
+       except: pass
