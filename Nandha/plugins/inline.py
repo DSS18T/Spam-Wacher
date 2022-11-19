77f737b3b69b24d6226ba239ea2292ae79c82794
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-19 22:31:44 +0530

    Update inline.py

--- Nandha/plugins/inline.py
+++ Nandha/plugins/inline.py
@@ -8,11 +8,12 @@ async def inline(_, query):
      user_id = query.from_user.id
      photos = []
      async for photo in Nandha.get_chat_photos(user_id):
-          photos.append(photo.file_id) 
-     await Nandha.answer_inline_query(
-         query.id,
-         results = [
+          photos.append(photo.file_id)
+     for photo in photos: 
+        await Nandha.answer_inline_query(
+          query.id,
+             results = [
             InlineQueryResultCachedPhoto(
                title = "Get Profile of Yours!",
                description = "Get Profile Photo",
-               photo_file_id=photos)], is_gallery=True, cache_time=2)
+               photo_file_id=photo)], is_gallery=True, cache_time=2)
