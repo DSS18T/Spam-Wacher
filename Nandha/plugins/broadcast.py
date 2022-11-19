f6d9e23956301b8938de1269ea7dcef4441f7ea6
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-19 06:20:50 +0530

    Update broadcast.py

--- Nandha/plugins/broadcast.py
+++ Nandha/plugins/broadcast.py
@@ -7,18 +7,34 @@ from Nandha.help.usersdb import *
 
 
 @Nandha.on_message(filters.user(config.OWNER_ID) & filters.command("groupcast",config.CMDS))
-async def global_cast(_, message):
+async def group_cast(_, message):
       reply = message.reply_to_message
       chat = message.chat  
       if not reply: return await message.reply("Reply to Message to Brodcast!")
-      success = "**Global Brodcast**:\n**Success**: `{}`\n**Failed**: `{}`"
-      msg = await message.reply("`Please wait Some Minutes!`", quote=True)
-      list_1 = get_chats()
-      list_2 = get_users()
+      success = "**Group Brodcast**:\n**Success**: `{}`\n**Failed**: `{}`"
+      msg = await message.reply("`Please wait Some Minutes!`", quote=True)      
       chat_id = []
-      for id in list_1:
+      for id in get_chats():
          chat_id.append(id)
-      for id in list_2:
+      done = 0
+      for ids in chat_id:
+          try:
+             await Nandha.copy_message(ids, chat.id, reply.id)
+             done +=+1
+             await asyncio.sleep(3)
+          except: fail = len(chat_id)-done    
+      return await msg.edit(success.format(done, fail))
+         
+       
+@Nandha.on_message(filters.user(config.OWNER_ID) & filters.command("usercast",config.CMDS))
+async def user_cast(_, message):
+      reply = message.reply_to_message
+      chat = message.chat  
+      if not reply: return await message.reply("Reply to Message to Brodcast!")
+      success = "**User Brodcast**:\n**Success**: `{}`\n**Failed**: `{}`"
+      msg = await message.reply("`Please wait Some Minutes!`", quote=True)      
+      chat_id = []
+      for id in get_users():
          chat_id.append(id)
       done = 0
       for ids in chat_id:
