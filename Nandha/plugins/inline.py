326b169089e74cc7ce4c77540e2488a9458e44ef
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-19 21:17:27 +0530

    Update inline.py

--- Nandha/plugins/inline.py
+++ Nandha/plugins/inline.py
@@ -3,18 +3,14 @@ from pyrogram.types import *
 from Nandha import Nandha
 
 
-Nandha_INFO = """
-Nandha is my owner
-Nandha birth is 2006
-Nandha is playboy
-Nandha don't have any ambition for his life!
-"""
-
-@Nandha.on_inline_query(filters.regex("^Nandha"))
+@Nandha.on_inline_query(filters.regex("^pfp"))
 async def inline(_, query):
-     await Nandha.answer_inline_query(
+     user_id = query.from_user.id
+     async for photo in Nandha.get_chat_photos(user_id):
+         await Nandha.answer_inline_query(
          query.id,
          results = [
-           InlineQueryResultArticle(
-               "Nandha",
-            InputTextMessageContent(Nandha_INFO))])
+            InlineQueryResultPhoto(
+               title = "Get Profile of Yours!",
+               description = "Get Profile Photo",
+               photo_file_id=photo.file_id)
