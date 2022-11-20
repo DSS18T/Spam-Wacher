41a58c1feb5c9acec2d19dbf9a4d4ff6e9497234
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-20 11:01:39 +0530

    Update inline.py

--- Nandha/plugins/inline.py
+++ Nandha/plugins/inline.py
@@ -2,10 +2,7 @@ from pyrogram import filters
 from pyrogram.types import *
 from Nandha import Nandha
 
-
-@Nandha.on_inline_query(filters.regex("^pfp"))
-async def inline(_, query):
-     user_id = query.from_user.id
+async def pfp_inline(user_id; int):
      answers = []
      async for photo in Nandha.get_chat_photos(user_id):
         answers.append(
@@ -14,6 +11,14 @@ async def inline(_, query):
                 photo_file_id=photo.file_id
             )
            )
+     return answers
 
-     await query.answer(answers, cache_time=30)
+
+@Nandha.on_inline_query()
+async def inline(_, query):
+     string = query.query.casefold()
+     user_id = query.from_user.id
+     if string.split()[0] == "pfp":
+          answers = await pfp_inline(user_id)
+          return await query.answer(answers, cache_time=30)
      
