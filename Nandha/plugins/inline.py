68223195ba8e63b5402004871513047068ec38b4
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-20 14:09:25 +0530

    Update inline.py

--- Nandha/plugins/inline.py
+++ Nandha/plugins/inline.py
@@ -1,10 +1,13 @@
 import time
+import os
 
 import config
 from pyrogram import filters
 from pyrogram.types import *
 from Nandha import Nandha, START_TIME
 from Nandha.help.helper_func import get_readable_time
+from icrawler.builtin import GoogleImageCrawler
+from subprocess import getoutput as run
 
 
 keywards = [
@@ -13,6 +16,23 @@ keywards = [
 "img",
 ]
 
+
+async def img_inline(search):
+    google_Crawler = GoogleImageCrawler(storage = {'root_dir': r'wall_images'})
+    google_Crawler.crawl(keyword = search, max_num = 6)
+    image = run("ls gg_images").split()
+    answers = []
+    
+    for link in image:
+        
+        answers.append(
+        InlineQueryResultDocument(
+           document_url = f"/app/img_images/{link}",
+           thumb_url = f"/app/img_images/{link},
+           caption = "Made by @NandhaBots"))
+    return answers
+    
+      
 
 async def pfp_inline(user_id: int):
      answers = []
@@ -66,5 +86,10 @@ async def inline(_, query):
      elif string.split()[0] == "ping":
           answers = await ping_inline()
           return await query.answer(answers)
+     elif string.split()[0] == "img":
+          search = string.split(string.split()[0])[1]
+          answers = await img_inline(search)
+          await query.answer(answers, cache_time=200)
+          return os.system("rm -rf wall_images")
      else:
           return await query.answer(results=[InlineQueryResultArticle("Error Raises!",InputTextMessageContent("Invalid Inline Command! 🧐"))],switch_pm_parameter="Invalid Inline!")
