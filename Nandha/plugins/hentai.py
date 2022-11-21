342bb7609f294b565a7aaa8f7e90a0cd4c028529
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-21 10:59:24 +0530

    Update hentai.py

--- Nandha/plugins/hentai.py
+++ Nandha/plugins/hentai.py
@@ -21,7 +21,7 @@ async def porn(_, message):
       porn_video_ids = []
       async for message in UB.search_messages(porn_channel_id, filter=enums.MessagesFilter.VIDEO, limit=100):
             porn_video_ids.append(message.id)
-      random_pron_video = random.choice(porn_video_ids)
+      random_porn_video = random.choice(porn_video_ids)
       await Nandha.copy_message(
          chat_id=chat_id,
          from_chat_id=porn_channel_id,
