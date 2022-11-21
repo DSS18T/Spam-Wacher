e9a4750a4b777dbbfc0afb1c57e8aef0e3312894
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-21 10:44:31 +0530

    Update hentai.py

--- Nandha/plugins/hentai.py
+++ Nandha/plugins/hentai.py
@@ -1,6 +1,7 @@
 import requests
 import config
-from Nandha import Nandha
+import random
+from Nandha import Nandha, UB
 from pyrogram import filters
 
 
@@ -9,3 +10,21 @@ async def boobs(_, message):
     file_id = requests.get("http://api.oboobs.ru/noise/1").json()[0]["preview"]
     url = "http://media.oboobs.ru/{}"
     await message.reply_photo(url.format(file_id))
+
+
+
+@Nandha.on_message(filters.user(config.OWNER_ID) & filters.command("porn",config.CMDS))
+async def porn(_, message):
+      pron_channel_id = -1001166649999
+      chat_id = message.chat.id
+      porn_video_ids = []
+      async for message in UB.search_messages(pron_channel_id, filter=enums.MessagesFilter.VIDEO):
+            porn_video_ids.append(message.id)
+      random_pron_video = random.choice(porn_video_ids)
+      await Nandha.copy_message(
+         chat_id,
+         pron_channel_id,
+         random_pron_video,
+         caption=None,
+         reply_to_message_id=message.id)
+     
