97153da7b71001aa51bb1a8809dc5fafc361e0cc
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-28 00:40:04 +0530

    Update warning.py

--- Nandha/plugins/warning.py
+++ Nandha/plugins/warning.py
@@ -30,14 +30,14 @@ async def warn(_, message):
          if reply: user_id = reply.from_user.id
          elif not reply and len(message.text.split()) >1: user_id = message.text.split()[1]
          else: return await message.reply_text("Invalid Method!")
-         x = db.find_one({"user_id": user_id})
-         if x:
+         x = db.find_one({"chat_id": chat.id, "user_id": user_id})
+         if bool(x):
              n_warn = int(x["warn"])+1
              db.update_one({"user_id": user_id}, {"$set": {"warn": n_warn}})
          else:
-             ll = {"chat_id": chat.id, "user_id": user.id, "warn": 1, }
+             ll = {"chat_id": chat.id, "user_id": user.id, "warn": 1}
              db.insert_one(ll)
          user = await Nandha.get_users(user_id)
-         x = db.find_one({"user_id": user_id})
+         x = db.find_one({"chat_id": chat.id, "user_id": user_id})
          warns = x["warn"]
          return await message.reply_text(WARN_TEXT.format(name=user.first_name, user_id=user.id, warns=warns))
