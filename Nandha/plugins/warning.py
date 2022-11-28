e96b3a34b55bc37d05f7e100a4fea246a108677d
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-28 06:25:41 +0530

    Update warning.py

--- Nandha/plugins/warning.py
+++ Nandha/plugins/warning.py
@@ -1,3 +1,4 @@
+import config
 
 from Nandha import (
 mongodb, Nandha, )
@@ -10,11 +11,18 @@ db = mongodb["WARNNING"]
 
 
 WARN_TEXT = """
-WARNNING ⚠️
+WARNING!
 name: {name}
 uid: [`{user_id}`]
-
-warns: [`{warns}`]
+warn by {admin}
+total warns: [`{warns}`]
+"""
+WARN_B_TEXT = """
+BANNED!
+name: {name}
+uid: [`{user_id}`]
+warn by {admin}
+total warns: [`{warns}`]
 """
 
 @Nandha.on_message(filters.command("warn"))
@@ -26,18 +34,27 @@ async def warn(_, message):
      elif await can_ban_members(chat_id=chat.id, user_id=user.id) == False:  
            return await message.reply_text("You Needs a can_restrict_members Rights!")
      else:
+         if await is_admin(chat_id=chat.id, user_id=config.BOT_ID) == False:
+              return await message.reply_text("Make Me Admin First Baka!")
+         elif await can_ban_members(chat_id=chat.id, user_id=config.BOT_ID) == False:  
+              return await message.reply_text("I Needs a can_restrict_members Rights!")
          reply = message.reply_to_message
          if reply: user_id = reply.from_user.id
          elif not reply and len(message.text.split()) >1: user_id = message.text.split()[1]
          else: return await message.reply_text("Invalid Method!")
          x = db.find_one({"chat_id": chat.id, "user_id": user_id})
-         if bool(x):
+         user = await Nandha.get_users(user_id)
+         if int(x["warn"]) == 3:
+              await Nandha.ban_chat_member(chat.id, user_id)
+              await message.reply_text(WARN_B_TEXT.format(name=user.mention, user_id=user.id,admin=message.from_user.mention, warns=warns))
+              return
+         elif bool(x):
              n_warn = int(x["warn"])+1
              db.update_one({"chat_id": chat.id, "user_id": user_id}, {"$set": {"warn": n_warn}})
          else:
              ll = {"chat_id": chat.id, "user_id": user_id, "warn": 1}
              db.insert_one(ll)
-         user = await Nandha.get_users(user_id)
          x = db.find_one({"chat_id": chat.id, "user_id": user_id})
          warns = int(x["warn"])
-         return await message.reply_text(WARN_TEXT.format(name=user.first_name, user_id=user.id, warns=warns))
+         return await message.reply_text(WARN_TEXT.format(name=user.mention, user_id=user.id,admin=message.from_user.mention warns=warns))
+return await message.reply_text(WARN_TEXT.format(name=user.mention, user_id=user.id,admin=message.from_user.mention, warns=warns))
