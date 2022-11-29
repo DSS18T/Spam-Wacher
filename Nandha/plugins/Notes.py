e2491d7fce532d06392170ad56dde705edb0d11d
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-30 05:03:36 +0530

    Update Notes.py

--- Nandha/plugins/Notes.py
+++ Nandha/plugins/Notes.py
@@ -16,27 +16,27 @@ async def save(_, message):
      try: note_name = message.text.split(None,1)[1].lower()
      except: return await message.reply_text("Give Note Name To Save!")
      if reply and reply.text:
-          db.insert_one({"chat_id": chat_id, "note_name": note_name, "text": reply.text})
+          db.insert_one({"chat_id": chat_id, "note_name": note_name, "text": reply.text}, "type": "text")
      elif reply and reply.video:
           if reply.caption:
                caption = reply.caption
           else: caption = ""
-          db.insert_one({"chat_id": chat_id, "note_name": note_name, "video": reply.video.file_id, "caption": caption})
+          db.insert_one({"chat_id": chat_id, "note_name": note_name, "video": reply.video.file_id, "caption": caption, "type": "video"})
      elif reply and reply.document:
           if reply.caption:
                caption = reply.caption
           else: caption = ""
-          db.insert_one({"chat_id": chat_id, "note_name": note_name, "document": reply.document.file_id, "caption": caption})
+          db.insert_one({"chat_id": chat_id, "note_name": note_name, "document": reply.document.file_id, "caption": caption, "type": "document"})
      elif reply and reply.animation:
           if reply.caption:
                caption = reply.caption
           else: caption = ""
-          db.insert_one({"chat_id": chat_id, "note_name": note_name, "animation": reply.animation.file_id, "caption": caption})
+          db.insert_one({"chat_id": chat_id, "note_name": note_name, "animation": reply.animation.file_id, "caption": caption, "type": "animation"})
      elif reply and reply.photo:
           if reply.caption:
                caption = reply.caption
           else: caption = ""
-          db.insert_one({"chat_id": chat_id, "note_name": note_name, "photo": reply.photo.file_id, "caption": caption})
+          db.insert_one({"chat_id": chat_id, "note_name": note_name, "photo": reply.photo.file_id, "caption": caption, "type": "photo"})
      return await message.reply_text("Added! `#{}`".format(note_name))
 
 @Nandha.on_message(filters.regex("^#"))
@@ -47,26 +47,26 @@ async def get_notes(_, message):
      except: return await message.reply_text("example: `#test`")
      x = db.find_one({"chat_id": chat_id, "note_name": note_name})
      if bool(x):
-          if x["video"]:
+          if "video" == x["type"]:
                 video = x["video"]
                 caption = x["caption"]
                 return await message.reply_video(video=video, caption=caption)
-          if x["animation"]:
+          elif "animation" == x["type"]:
                 animation = x["animation"]
                 caption = x["caption"]
                 return await message.reply_animation(animation=animation, caption=caption)
-          if x["photo"]:
+          elif "photo" == x["type"]:
                 photo = x["photo"]
                 caption = x["caption"]
                 return await message.reply_photo(photo=photo, caption=caption)
-          if x["document"]:
+          elif "document" == x["type"]:
                 document = x["document"]
                 caption = x["caption"]
                 return await message.reply_document(document=document, caption=caption)
-          if x["text"]:
+          elif "text" == x["type"]:
                 text = x["text"]              
                 return await message.reply_text(text=text)
-          else: return await message.reply_text("No notes saved in >`{}`<".format(note_name))
+          else: return await message.reply_text("can't send this note in >`{}`<".format(note_name))
      else: return await message.reply_text("No notes saved in >`{}`<".format(note_name))
 
 
