988d76fe9b8d3a44b9547e4e7897b8c4e12be9b2
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-11-30 05:16:19 +0530

    Update Notes.py

--- Nandha/plugins/Notes.py
+++ Nandha/plugins/Notes.py
@@ -4,8 +4,21 @@ from Nandha import mongodb, Nandha
 from pyrogram import filters
 from pyrogram.types import *
 from pyrogram import enums
+from Nandha.help.admin import *
 
 db = mongodb["NOTES"]
+
+
+@Nandha.on_message(filters.command("clear",config.CMDS))
+async def clear(_, message):
+      chat_id = message.chat.id
+      try: note_name = message.text.split(None,1)[1]
+      except: return await message.reply_text("what I want do clear? tell me note name!")
+      x = db.find_one({"chat_id": chat_id, "note_name": note_name})
+      if bool(x):
+          db.delete_one(x)
+          return await message.reply_text(f"Deleted! > `{note_name}` <")
+      return await message.reply_text("No Notes In This > `{note_name}` <")
 
 @Nandha.on_message(filters.command("save",config.CMDS))
 async def save(_, message):
@@ -13,6 +26,7 @@ async def save(_, message):
      chat_id = message.chat.id
      user_id = message.from_user.id
      if message.chat.type == enums.ChatType.PRIVATE: return await message.reply_text("Commands Work Only On Groups!")
+     elif await is_admin(chat_id, user_id) == False: return await message.reply_text("Admins Only Can Save Notes!")
      try: note_name = message.text.split(None,1)[1].lower()
      except: return await message.reply_text("Give Note Name To Save!")
      if reply and reply.text:
