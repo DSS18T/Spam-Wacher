2dda5c185ca52a18123a0725a96566386cde05bf
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-12-05 21:34:40 +0530

    Update afk.py

--- Nandha/plugins/afk.py
+++ Nandha/plugins/afk.py
@@ -15,6 +15,13 @@ from pyrogram.types import *
 db = mongodb.AFK
 
 
+def afk_users():
+    afk_users = []
+    for find in db.find():
+        afk_users.append(find["user_id"])
+    return afk_users
+
+
 @Nandha.on_message(group=20)
 async def AFK(_, message):
     text = message.text
@@ -31,12 +38,8 @@ async def AFK(_, message):
            return await message.reply_text(f"Bye {name} Take A Rest! 👻")
     except: pass
     
-    afk_users = []
-
-    for find in db.find():
-         afk_users.append(find["user_id"])
-
-    if message.from_user.id in afk_users:
+    
+    if message.from_user.id in afk_users():
           find = db.find_one({"user_id": user_id})
           db.delete_one(find)
           return await message.reply_text(f"Welcome Back {name} 🌚!")
@@ -44,15 +47,11 @@ async def AFK(_, message):
 @Nandha.on_message( group=20)
 async def afk_s(_, message):
 
-    afk_users = []
-
-    for find in db.find():
-         afk_users.append(find["user_id"])
-
+    
     reply = message.reply_to_message
     reply_uid = reply.from_user.id
     reply_uname = reply.from_user.first_name
     
 
-    if reply and reply_uid in afk_users:
+    if reply and reply_uid in afk_users():
            await message.reply("his offline!")
