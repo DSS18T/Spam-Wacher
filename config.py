eaa3a607d4036b98e589b986a2e9145079280e72
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2023-01-16 07:06:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,23 @@
-#group and channels
+#group & channels and bot username 
 GROUP_LINK = "t.me/NandhaSupport"
-CHANNEL_LINK = "t.me/nandhabots"
+CHANNEL_LINK = "t.me/Nandhabots"
+USERNAME = "VegetaRobot"
+
+#group & channels and bot I'd
+
 LOG_CHANNEL_ID = -1001865726272
 GROUP_ID = -1001717881477
 CHANNEL = -1001566403901
-USERNAME = "SpamWatcherBot"
 OWNER_ID = 5696053228
-DEVS = [5696053228,5446911]
-
 BOT_ID = 5561055962
+DEVS = [5696053228]
+
+
 CMDS = ["/","!",".","?","$",]
 
-profile = "http://telegra.ph/file/622ab6e6cc673a7a26266.mp4"
+profile = "https://i.imgur.com/HGlOm7Q.jpeg"
 
 APP_ID = 13257951
 APP_HASH = "d8ea642aedb736d40035bc05f0cfd477"
-TOKEN = "5561055962:AAH6aRTPa30nBk7kKfqZ336D9rcuwGKMRi8"
+TOKEN = "2128359921:AAEWCm5u8avaqii-aXO318YfjLdLNE0RpOI"
 SESSION = "BQDKTN8AqQWEwZPnhHEQSxr-9qm5lacod-CtbehbAy4DdKy93u8pQPODGHROY8FWk9VSwPr_F8IflQXd8DhpUFFEGt9FpG0QhqBm9jsOdZ4Pt60ceRYX-TDBdq-8VId5Jk2xZDdf3sBFVT8YRp_qVXIrFavEvr3EaeoByR9dH-V-KIvp154WW5WJhNqrjVSSWcR6D-h2S1AH-hiouGXARYX8GVJL1UeZJzCqPro_QjwCXZMBBynN5i3V3xtV1UEFtyrwv3uxrqy5BX9_ZVaEOGNZAT6gQ96rhADjLmR1mZgceI4mjNGFsHzf0x35F0Zj4KL-43Nxj50EBpJLsMuYixBC3h3nXQAAAAFTgt_sAA"
