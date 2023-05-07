0b87035cabfb0e6c8414065026cb22f630af1a6f
Darsh <darshshetty04@gmail.com>
2023-05-07 22:33:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,22 @@
 #group & channels and bot username 
-GROUP_LINK = "t.me/NandhaSupport"
-CHANNEL_LINK = "t.me/Nandhabots"
-USERNAME = "spamwatcherBot"
+GROUP_LINK = "t.me/GOKUSUPPORTCHAT"
+CHANNEL_LINK = "t.me/BROTHERSTILLDEATHS"
+USERNAME = "NobaraXrobot"
 
 #group & channels and bot I'd
 
-LOG_CHANNEL_ID = -1001865726272
-GROUP_ID = -1001717881477
-CHANNEL = -1001566403901
-OWNER_ID = 5696053228
-BOT_ID = 5561055962
-DEVS = [5696053228]
+LOG_CHANNEL_ID = -1001959136748
+GROUP_ID = -1001709409711
+CHANNEL = -1001983353439
+OWNER_ID = 1448400310
+BOT_ID = 5930553271
+DEVS = [5140306824]
 
 
 CMDS = ["/","!",".","?","$",]
 
-profile = "https://graph.org/file/45bc8c1c0ff800e84a56f.jpg"
+profile = "https://te.legra.ph/file/0e218a33897370f71eda8.jpg"
 
-APP_ID = 
-APP_HASH = ""
-TOKEN = ""
+APP_ID = 6212330
+APP_HASH = "1dcf154704672a8c279126e1ecf229c3"
+TOKEN = "5930553271:AAG18uoOwAYZKIcI1fq_a23REFZrs9MsXS8"
